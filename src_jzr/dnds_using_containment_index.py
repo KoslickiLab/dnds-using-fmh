4b16c18fa797e8f2f5335780c987e4e7679f0614
Judith Rodriguez <judiths.r91@gmail.com>
2023-01-19 09:10:24 -0500

    add functions that report highest and second highest containment index

--- src_jzr/dnds_using_containment_index.py
+++ src_jzr/dnds_using_containment_index.py
@@ -29,25 +29,32 @@ def main(args):
 
     if os.path.exists(results+"ORFs_samples.faa"):
         print("Running sourmash sketch...",time.time())
-        predictORF.sketch(genome, results+"ORFs_samples.faa",kmer_size=kmers,outputfile1=results+"ref-genome.sig", outputfile2=results+"samples.sig.zip")
+        predictORF.sketch(genome, results+"ORFs_samples.faa",kmer_size=kmers,ref_sig=results+"refref-genome7-70.sig.zip", query_sig=results+"orfs7-70.sig.zip")
         cmd3 = f"unzip {results}samples.sig.zip" #produces SOURMASH-MANIFEST.csv
         subprocess.run(cmd3, stdout=subprocess.PIPE, shell=True)
     else:
         print("File does not exist. Stop analysis.")
 
-    if os.path.exists(results+"SOURMASH-MANIFEST.csv") and os.path.exists(results+"samples.sig.zip") and os.path.exists(results+"ref-genome.sig"):
-        print("Running sourmash search...",time.time())
-        cmd4 = f"mkdir {results}md5"
-        subprocess.run(cmd4, stdout=subprocess.PIPE, shell=True)
-        predictORF.search(MANIFEST_CSV_FILE=results+"SOURMASH-MANIFEST.csv", sig1=results+"ref-genome.sig", sig2=results+"samples.sig.zip",output_directory=results+"md5/")
-    else:
-        print("File does not exist. Stop analysis.")
+    #if os.path.exists(results+"SOURMASH-MANIFEST.csv") and os.path.exists(results+"samples.sig.zip") and os.path.exists(results+"ref-genome.sig"):
+    #    print("Running sourmash search...",time.time())
+    #    cmd4 = f"mkdir {results}md5"
+    #    subprocess.run(cmd4, stdout=subprocess.PIPE, shell=True)
+    #    predictORF.search(MANIFEST_CSV_FILE=results+"SOURMASH-MANIFEST.csv", ref_sig=results+"ref-genome.sig", query_sig=results+"samples.sig.zip",output_directory=results+"md5/")
+    #else:
+    #    print("File does not exist. Stop analysis.")
 
-    if os.path.exists(results+"samples.sig.zip") and os.path.exists(results+"ref-genome.sig"):
+    if os.path.exists(results+"orfs7-70.sig.zip"):
         print("Running sourmash prefetch...",time.time())
-        predictORF.prefetch(sig1=results+"ref-genome.sig", sig2=results+"samples.sig.zip",kmer_size=kmers)
+        predictORF.prefetch(query_sig=results+"orfs7-70.sig.zip", ref_sig=results+"ref-genome7-70.sig.zip",kmer_size=kmers)
     else:
         print("File does not exist. Stop analysis.")
+        print(os.path.exists(results+"orfs7-70.sig.zip"))
+        print(os.path.exists(results+"ref-genome7-70.sig.zip"))
+
+    # report highest and second highest containment index in dictionary pickle file
+    reportCI.produce_containment_csv(wd=results)
+    reportCI.prep(data=results+'results_kmers.csv',output=results+"containment.csv")
+    reportCI.analysis(data=results+"containment.csv",output=results+"CIdict.pickle")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
