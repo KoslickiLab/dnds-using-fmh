dc78e0f89eda434abd0c256f44bbc475b305172b
Judith Rodriguez <judiths.r91@gmail.com>
2023-01-27 16:19:12 -0500

    modified arguments for type of analysis

--- src_jzr/dnds_using_containment_index.py
+++ src_jzr/dnds_using_containment_index.py
@@ -18,8 +18,9 @@ def main(args):
     kmers = args.k
     results = args.wd
     sd1 = args.scaled1
-    mltyp = args.moltype
-    trnslt = args.translate
+    analysis_type = args.analyze
+#    mltyp = args.moltype
+#    trnslt = args.translate
     
     #if args.predict == "orfs":
     #    """The fasta input file does not have open reading frames identified"""
@@ -29,21 +30,23 @@ def main(args):
     #    """The first fasta file does not have open reading frames identified"""
     #    print("Getting ORFs for sample input",time.time())
     #    findORFs.ORFs_file(samples, results+"ORFs_samples.faa")
-    if args.moltype == 'protein':
-        print('Analyzing protein sequences')
-        if args.predict == "frame":
+#    if args.moltype == 'protein':
+#        print('Analyzing protein sequences')
+    if args.analyze == 'frame_predict':
             """Create fasta file with six reading frames of each sequence"""
             print("Obtain six reading frames for each query")
             findORFs.reading_frames_file(samples, results+"query_frames.faa")
             query = results+"query_frames.faa"
-    elif args.moltype == 'dna':
+#    elif args.moltype == 'dna':
+    elif args.analyze == "dna" or args.analyze == "protein":
             print('Analyzing DNA sequences')
             query = samples
 
     print('Ready to sketch!')
     if os.path.exists(query):
         print("Running sourmash sketch...",time.time())
-        predictORF.sketch(genome, query,kmer_size=kmers,ref_output=results+"ref-genome.sig", query_output=results+"queries.sig.zip",moltype=mltyp,translate=trnslt)
+#        predictORF.sketch(genome, query,kmer_size=kmers,ref_output=results+"ref-genome.sig", query_output=results+"queries.sig.zip",moltype=mltyp,translate=trnslt)
+        predictORF.sketch(genome, query,kmer_size=kmers,ref_output=results+"ref-genome.sig", query_output=results+"queries.sig.zip",analysis=analysis_type)
         #cmd3 = f"unzip {results}queries.sig.zip" #produces SOURMASH-MANIFEST.csv How do I output to a directory
         #subprocess.run(cmd3, stdout=subprocess.PIPE, shell=True)
     #if os.path.exists(results+samples):
@@ -66,7 +69,8 @@ def main(args):
     print('Ready to prefetch!')
     if os.path.exists(results+"queries.sig.zip"):
         print("Running sourmash prefetch...",time.time())
-        predictORF.prefetch(query_sig="queries.sig.zip", ref_sig="ref-genome.sig",kmer_size=kmers,wd=results)
+#        predictORF.prefetch(query_sig="queries.sig.zip", ref_sig="ref-genome.sig",kmer_size=kmers,wd=results,moltype=mltyp)
+        predictORF.prefetch(query_sig="queries.sig.zip", ref_sig="ref-genome.sig",kmer_size=kmers,wd=results,analysis=analysis_type)
     else:
         print("File does not exist. Stop analysis.")
         print(os.path.exists(results+"queries.sig.zip"))
@@ -144,18 +148,25 @@ if __name__ == "__main__":
         help = 'Output files from sourmash program'
     )
 
-    parser.add_argument(
-        '--moltype',
-        default = 'protein',
-        choices = ['protein','dna'],
-        help = 'Indicate if protein or DNA sequences are being used'
-    )
+#    parser.add_argument(
+#        '--moltype',
+#        default = 'protein',
+#        choices = ['protein','dna'],
+#        help = 'Indicate if protein or DNA sequences are being used'
+#    )
+
+#    parser.add_argument(
+#        '--translate',
+#        default = 'no',
+#        choices = ['yes','no'],
+#        help = 'Indicate we are translating both ref and query sequences.'
+#    )
 
     parser.add_argument(
-        '--translate',
-        default = 'no',
-        choices = ['yes','no'],
-        help = 'Indicate we are translating both ref and query sequences.'
+        '--analyze',
+        default = 'frame_predict',
+        choices = ['frame_predict','dna','protein'],
+        help = 'type of sourmash analysis'
     )
 
     args = parser.parse_args()
