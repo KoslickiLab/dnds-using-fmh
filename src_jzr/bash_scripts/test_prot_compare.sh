bfbc6d21aeb18b13a14bb977fd8f8f045727efb5
Judith Rodriguez <judiths.r91@gmail.com>
2023-01-28 15:53:49 -0500

    added multiple ksizes for 9 sequences

--- src_jzr/bash_scripts/test_prot_compare.sh
+++ src_jzr/bash_scripts/test_prot_compare.sh
@@ -15,10 +15,13 @@ ref_scaled=1
 samp_scaled=1
 tbp=1
 
-sourmash sketch protein -p k=7,scaled=$ref_scaled $ref -o $ref_output
+sourmash sketch protein -p k=7,k=14,k=21,k=28,k=35,k=42,k=49,k=56,k=63,k=70,scaled=$ref_scaled $ref -o $ref_output
 
-sourmash sketch protein -p k=7,scaled=$samp_scaled $samples --singleton -o $samp_output
+sourmash sketch protein -p k=7,k=14,k=21,k=28,k=35,k=42,k=49,k=56,k=63,k=70,scaled=$samp_scaled $samples --singleton -o $samp_output
 
-K=7
-nohup sourmash compare $ref_output $samp_output --containment --protein --o ${wd}compare${K}.mat --ksize $K > ${wd}compare${K}.txt 2>&1 & #assign to diff log files
+for K in 7 14 21 28 35 42 49 56 62 70 
+do
 
+    nohup sourmash compare $ref_output $samp_output --containment --protein --o ${wd}compare${K}.mat --ksize $K > ${wd}compare${K}.txt 2>&1 & #assign to diff log files
+
+done
