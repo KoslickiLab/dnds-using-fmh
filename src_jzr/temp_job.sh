e78977fe02304ececd509cfc112b6e2ad8b5de00
Judith Rodriguez <judiths.r91@gmail.com>
2023-01-28 09:04:26 -0500

    bash script to obtain results for frame analysis

--- src_jzr/temp_job.sh
+++ src_jzr/temp_job.sh
@@ -1,24 +1,23 @@
-#rm /home/grads/jzr5814/data/tests/prefetch.txt
-#rm /home/grads/jzr5814/data/tests/*.sig*
-#rm /home/grads/jzr5814/data/tests/results_kmers.csv
-#rm /home/grads/jzr5814/data/tests/SOURMASH-MANIFEST.csv
-#rm /home/grads/jzr5814/data/tests/prefetch_res_*.csv
-#rm test_sketch_ref.txt test_sketch_sample.txt test_prefetch*.txt
+#!/bin/bash
+set -eoux pipefail
 
-ref=../../data/tests/50_sequences.fna 
-samples=../../data/tests/5_uniprot_seqs.fasta 
-ref_output=../../data/test3/50_sequences.sig
-samp_output=../../data/test3/5_uniprot_seqs.sig.zip
+#results from this are found in /data/jzr5814/data/frame_analysis_using_bash_script
+#Decided to run abashscript because the python program has issues
+
+ref=../../data/uniprotkb.fasta
+samples=../../data/query_frames.faa #found in /data/jzr5814/data/frame_analysis_using_bash_script
+ref_output=../../data/uniprotkb.sig #found in /data/jzr5814/data/
+samp_output=../../data/query_frames.sig.zip
 ref_scaled=100
 samp_scaled=1
 tbp=1
-wd=../../data/test3/
+wd=../../data/
 
-nohup sourmash sketch protein -p k=7,k=14,k=21,k=28,scaled=$ref_scaled $ref -o $ref_output > test_sketch_ref.txt 2>&1 &
+sourmash sketch protein -p k=7,k=14,k=21,k=28,k=35,k=42,k=49,k=56,k=63,k=70,scaled=$ref_scaled $ref -o $ref_output
 
-nohup sourmash sketch protein -p k=7,k=14,k=21,k=28,scaled=$samp_scaled $samples --singleton -o $samp_output > test_sketch_sample.txt 2>&1 &
+sourmash sketch protein -p k=7,k=14,k=21,k=28,k=35,k=42,k=49,k=56,k=63,k=70,scaled=$samp_scaled $samples --singleton -o $samp_output
 
-for K in 7 14 21 28
+for K in 7 14 21 28 35 42 49 56 63 70
 do
 	echo $K
 	nohup sourmash prefetch $ref_output $samp_output --protein --o ${wd}prefetch_res_${K}.csv --threshold-bp $tbp --ksize $K > test_prefetch${K}.txt 2>&1 & #assign to diff log files
