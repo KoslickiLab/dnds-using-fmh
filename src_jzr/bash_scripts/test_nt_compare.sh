46486b2ed47bcd96d27108315bb2c7c2c90b3d6a
Judith Rodriguez <judiths.r91@gmail.com>
2023-01-31 13:15:17 -0500

    add spurmash prefetch command for just one ksize to test a simple sequence

--- src_jzr/bash_scripts/test_nt_compare.sh
+++ src_jzr/bash_scripts/test_nt_compare.sh
@@ -2,27 +2,29 @@
 set -eoux pipefail
 
 #files for a simple sequence
-#wd=/data/jzr5814/data/tests/nt/simple/
+wd=/data/jzr5814/data/tests/nt/simple/
 #files for a single sequence
 #wd=/data/jzr5814/data/tests/nt/single_seq/
 #files for a 9 sequences
-wd=/data/jzr5814/data/tests/nt/9_seqs/
+#wd=/data/jzr5814/data/tests/nt/9_seqs/
 ref=${wd}ref.fna
 samples=${wd}query.fna 
 ref_output=${wd}ref.sig 
 samp_output=${wd}query.sig.zip
 ref_scaled=1
 samp_scaled=1
-tbp=1
+K=7
 
 sourmash sketch dna -p k=7,k=14,k=21,k=28,k=35,k=42,k=49,k=56,k=63,k=70,scaled=$ref_scaled $ref -o $ref_output
 
 sourmash sketch dna -p k=7,k=14,k=21,k=28,k=35,k=42,k=49,k=56,k=63,k=70,scaled=$samp_scaled $samples --singleton -o $samp_output
 
-for K in 7 14 21 28 35 42 49 56 62 70 
-do
-    nohup sourmash compare $ref_output $samp_output --containment --dna --o ${wd}compare${K}.mat --ksize $K > ${wd}compare${K}.txt 2>&1 & #assign to diff log files
+nohup sourmash compare $ref_output $samp_output --containment --dna --o ${wd}compare${K}.mat --csv ${wd}compare${K}.csv  --ksize $K > ${wd}compare${K}.txt 2>&1 &
 
-done
+#for K in 7 14 21 28 35 42 49 56 62 70 
+#do
+#    nohup sourmash compare $ref_output $samp_output --containment --dna --o ${wd}compare${K}.mat --csv ${wd}compare${K}.csv  --ksize $K > ${wd}compare${K}.txt 2>&1 & #assign to diff log files
+
+#done
 
 
