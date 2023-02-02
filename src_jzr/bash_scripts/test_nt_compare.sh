38ff5de9398644550b9f17cf58f4f5587a3ae37c
Judith Rodriguez <judiths.r91@gmail.com>
2023-02-02 12:24:32 -0500

    changed ksizes and reran analysis

--- src_jzr/bash_scripts/test_nt_compare.sh
+++ src_jzr/bash_scripts/test_nt_compare.sh
@@ -2,9 +2,9 @@
 set -eoux pipefail
 
 #files for a simple sequence
-wd=/data/jzr5814/data/tests/nt/simple/
+#wd=/data/jzr5814/data/tests/nt/simple/
 #files for a single sequence
-#wd=/data/jzr5814/data/tests/nt/single_seq/
+wd=/data/jzr5814/data/tests/nt/single_seq/
 #files for a 9 sequences
 #wd=/data/jzr5814/data/tests/nt/9_seqs/
 ref=${wd}ref.fna
@@ -15,16 +15,17 @@ ref_scaled=1
 samp_scaled=1
 K=7
 
-sourmash sketch dna -p k=7,k=14,k=21,k=28,k=35,k=42,k=49,k=56,k=63,k=70,scaled=$ref_scaled $ref -o $ref_output
+sourmash sketch dna -p k=5,k=10,k=15,k=20,scaled=$ref_scaled $ref -o $ref_output
 
-sourmash sketch dna -p k=7,k=14,k=21,k=28,k=35,k=42,k=49,k=56,k=63,k=70,scaled=$samp_scaled $samples --singleton -o $samp_output
+sourmash sketch dna -p k=5,k=10,k=15,k=20,scaled=$samp_scaled $samples --singleton -o $samp_output
 
-nohup sourmash compare $ref_output $samp_output --containment --dna --o ${wd}compare${K}.mat --csv ${wd}compare${K}.csv  --ksize $K > ${wd}compare${K}.txt 2>&1 &
+#nohup sourmash compare $ref_output $samp_output --containment --dna --o ${wd}compare${K}.mat --csv ${wd}compare${K}.csv  --ksize $K > ${wd}compare${K}.txt 2>&1 &
 
-#for K in 7 14 21 28 35 42 49 56 62 70 
-#do
-#    nohup sourmash compare $ref_output $samp_output --containment --dna --o ${wd}compare${K}.mat --csv ${wd}compare${K}.csv  --ksize $K > ${wd}compare${K}.txt 2>&1 & #assign to diff log files
+#for K in 7 14 21 28 35 42 49 56 62 70
+for K in 5 10 15 20 
+do
+    nohup sourmash compare $ref_output $samp_output --containment --dna --o ${wd}compare${K}.mat --csv ${wd}compare${K}.csv  --ksize $K > ${wd}compare${K}.txt 2>&1 & #assign to diff log files
 
-#done
+done
 
 
--- src_jzr/bash_scripts/test_prot_compare.sh
+++ src_jzr/bash_scripts/test_prot_compare.sh
@@ -2,11 +2,11 @@
 set -eoux pipefail
 
 #files for a simple sequence
-#wd=/data/jzr5814/data/tests/prot/simple/
+wd=/data/jzr5814/data/tests/prot/simple/
 #files for a single sequence
 #wd=/data/jzr5814/data/tests/prot/single_seq/
 #files for a 9 sequences
-wd=/data/jzr5814/data/tests/prot/9_seqs/
+#wd=/data/jzr5814/data/tests/prot/9_seqs/
 ref=${wd}ref.fna
 samples=${wd}query.fna 
 ref_output=${wd}ref.sig 
@@ -15,17 +15,18 @@ ref_scaled=1
 samp_scaled=1
 #K=7
 
-#Input are protein sequences
+#Input are protein sequencess
 #sourmash sketch protein -p k=7,k=14,k=21,k=28,k=35,k=42,k=49,k=56,k=63,k=70,scaled=$ref_scaled $ref -o $ref_output
 #sourmash sketch protein -p k=7,k=14,k=21,k=28,k=35,k=42,k=49,k=56,k=63,k=70,scaled=$samp_scaled $samples --singleton -o $samp_output
 
 #Input are DNA sequences
-sourmash sketch translate -p k=7,k=14,k=21,k=28,k=35,k=42,k=49,k=56,k=63,k=70,scaled=$ref_scaled $ref -o $ref_output
-sourmash sketch translate -p k=7,k=14,k=21,k=28,k=35,k=42,k=49,k=56,k=63,k=70,scaled=$samp_scaled $samples --singleton -o $samp_output
+sourmash sketch translate -p k=5,k=10,k=15,k=20,scaled=$ref_scaled $ref -o $ref_output
+sourmash sketch translate -p k=5,k=10,k=15,k=20,scaled=$samp_scaled $samples --singleton -o $samp_output
 
 #nohup sourmash compare $ref_output $samp_output --containment --protein --o ${wd}compare${K}.mat --csv ${wd}compare${K}.csv  --ksize $K > ${wd}compare${K}.txt 2>&1 &
 
-for K in 7 14 21 28 35 42 49 56 62 70 
+#for K in 7 14 21 28 35 42 49 56 62 70 
+for K in 5 10 15 20 
 do
 
     nohup sourmash compare $ref_output $samp_output --containment --protein --o ${wd}compare${K}.mat --csv ${wd}compare${K}.csv --ksize $K > ${wd}compare${K}.txt 2>&1 & #assign to diff log files
