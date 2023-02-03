3f39149d16b3d7d88add0e24d92cfb77a4004f71
Judith Rodriguez <judiths.r91@gmail.com>
2023-02-03 18:53:51 -0500

    test for debugging int type issue in ksize column

--- src_jzr/tests/test_reportCI.py
+++ src_jzr/tests/test_reportCI.py
@@ -6,7 +6,6 @@ from dNdS.reportCI import grab_containment_from_mat
 
 def test_grab_containment_from_mat():
     """Test grab_containment_from_mat function"""
-    """In this test, object for int is chaning type so come back to this for this test"""
     mat = pd.DataFrame(columns=['ref','q1'])
     mat = pd.concat([mat, pd.DataFrame.from_records([{'ref':1.0,'q1':0.0}])])
     mat = pd.concat([mat, pd.DataFrame.from_records([{'ref':0.0,'q1':1.0}])])
@@ -15,5 +14,3 @@ def test_grab_containment_from_mat():
     df = pd.DataFrame(output,columns=['A','B','containment','ksize'])
 
     assert grab_containment_from_mat(mat,7).equals(df)
-
-
