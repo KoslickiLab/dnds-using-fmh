a090a0e9913ca58147993d2bab7f5696e076e2ff
Judith Rodriguez <judiths.r91@gmail.com>
2023-02-03 11:24:18 -0500

    debugging grab_containment_from_mat() via pytest from test_reportCI.py

--- src_jzr/dNdS/reportCI.py
+++ src_jzr/dNdS/reportCI.py
@@ -134,3 +134,16 @@ def analysis_wrongFrames(data,output="CIdict_wrongframes.pickle"):
     with open(output, 'wb') as handle:
         pickle.dump(dictionary, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
+def grab_containment_from_mat(mat,ksize):
+    """This function converts matrix into df removes pairwise information"""
+    """When running contained_by() from sourmash api, a matrix via a csv file called compare.csv is produced"""
+    df = pd.DataFrame(columns=['A','B','containment','ksize'],dtype=int)
+    for i in range(len(mat)):
+        for j in range(len(mat)):
+            if i > j:
+                containment = mat.iloc[j,i]
+                df = pd.concat([df, pd.DataFrame.from_records([{'A':mat.columns[i],
+                'B':mat.columns[j],
+                'containment':containment,
+                'ksize':ksize}])],join='inner')
+    return(df)
