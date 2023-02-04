dd6934ddfefb9c472b5ded6ce33f50ce88c95c87
Judith Rodriguez <judiths.r91@gmail.com>
2023-02-03 19:05:10 -0500

    scaling queries to 100 not reference

--- src_jzr/sourmash_api_frame_cont.py
+++ src_jzr/sourmash_api_frame_cont.py
@@ -3,19 +3,19 @@ import pandas as pd
 import sourmash as sm
 
 #Creating signature of my ref sig to be the same scaled value of my query sig
-ref_input='/data/jzr5814/data/uniprotkb.fasta'
-scaled=1
-ref_output='/data/jzr5814/data/uniprotkb_scaled1.sig'
-cmd2=f"sourmash sketch protein -p {ref_input} k=7,k=14,k=21,k=28,k=35,k=42,k=49,k=56,k=63,k=70,scaled={scaled} -o {ref_output}"
-print(cmd2)
-subprocess.run(cmd2, stdout=subprocess.PIPE, shell=True)
+#query_input='/data/jzr5814/data/frame_analysis_using_bash_script/query_frames.faa'
+#scaled=100
+#query_output='/data/jzr5814/data/frame_analysis_using_bash_script/query_frames_scaled100.sig.zip'
+#cmd2=f"sourmash sketch protein {query_input} -p k=7,k=14,k=21,k=28,k=35,k=42,k=49,k=56,k=63,k=70,scaled={scaled} --singleton -o {query_output}"
+#print(cmd2)
+#subprocess.run(cmd2, stdout=subprocess.PIPE, shell=True)
 
 #output containment csv file
-query_file = '/data/jzr5814/data/frame_analysis_using_bash_script/query_frames.sig.zip'
-ref_file = '/data/jzr5814/data/uniprotkb_scaled1.sig'
+query_file = '/data/jzr5814/data/frame_analysis_using_bash_script/query_frames_scaled100.sig.zip'
+ref_file = '/data/jzr5814/data/uniprotkb.sig'
 
 ksize_lst = [7,14,21,28,35,42,49,56,63,70]
-
+ 
 df=pd.DataFrame(columns=['query','frame','ksize','containment'])
 
 for k in ksize_lst:
