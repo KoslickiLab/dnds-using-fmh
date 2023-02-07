dd16aae5796c87ca6a99f94c0061451f23814449
Judith Rodriguez <judiths.r91@gmail.com>
2023-02-07 09:44:16 -0500

    modified for new analysis

--- src_jzr/bash_scripts/sketch_job.sh
+++ src_jzr/bash_scripts/sketch_job.sh
@@ -9,6 +9,6 @@ wd=/data/jzr5814/data/frame_analysis_using_bash_script/compare_by_multiple_jobs/
 ref_output=${wd}uniprotkb_test2.sig #found in /data/jzr5814/data/
 samp_output=${wd}query_frames_test2.sig.zip
 
-nohup sourmash sketch protein -p k=7,k=14,k=15,k=16,k=17,k=18,k=19,k=20,k=21,scaled=$ref_scaled $ref -o $ref_output > ${wd}sketch_ref_log.txt 2>&1 &
+nohup sourmash sketch protein -p k=21,k=22,k=23,k=24,k=25,k=26,k=27,k=28,scaled=$ref_scaled $ref -o $ref_output > ${wd}sketch_ref_log.txt 2>&1 &
 
-nohup sourmash sketch protein -p k=7,k=14,k=15,k=16,k=17,k=18,k=19,k=20,k=21,scaled=$samp_scaled $samples --singleton -o $samp_output > ${wd}sketch_protein_log.txt 2>&1 &
+nohup sourmash sketch protein -p k=21,k=22,k=23,k=24,k=25,k=26,k=27,k=28,scaled=$samp_scaled $samples --singleton -o $samp_output > ${wd}sketch_protein_log.txt 2>&1 &
