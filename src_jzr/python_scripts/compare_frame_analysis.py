2f96fafb903ddb31b808a7b6a14a980f62aa4b64
Judith Rodriguez <judiths.r91@gmail.com>
2023-02-07 09:43:19 -0500

    obtain containment csv file from sourmash compare results

--- src_jzr/python_scripts/compare_frame_analysis.py
+++ src_jzr/python_scripts/compare_frame_analysis.py
@@ -3,7 +3,7 @@ import csv
 import pickle, os, glob, subprocess
 
 def frame(x):
-    return(x[-1:])
+    return('frame_'+x[-1:])
 
 wd = '/data/jzr5814/data/frame_analysis_using_bash_script/compare_by_multiple_jobs/test1/'
 
@@ -11,9 +11,8 @@ ksizes = [7,14,21,28,35,42,49,56,63,70]
 
 for ksize in ksizes:
     df = pd.read_csv(wd+'compare'+str(ksize)+'.csv', sep=',')
-    transpose_df=df.T.iloc[:,0].reset_index().rename(columns={'index':'query',0:'containment'}) #extract column where all queries are compared to ref
+    transpose_df=df.T.iloc[:,0].reset_index().rename(columns={'index':'query',0:'containment'}).rename_axis('index') #extract column where all queries are compared to ref
     transpose_df['ksize'] = ksize
-#    transpose_df['frame'] = transpose_df["query"][-1:]
 
     transpose_df["frame"] = transpose_df["query"].astype(str).apply(frame)
 
