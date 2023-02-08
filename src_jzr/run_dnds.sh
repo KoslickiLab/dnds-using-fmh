7d1271aa05cc464da8a5655ef45f43946d073e76
Judith Rodriguez <judiths.r91@gmail.com>
2023-02-08 13:35:18 -0500

    remove and renaming files

--- src_jzr/run_dnds.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eoux pipefail
-
-#Test run on small dataset
-#nohup python3 dnds_using_containment_index.py --ref_fasta ../../data/tests/50_sequences.fna --query_fasta ../../data/tests/5_nt_sequences.fna --predict frame --k 7,14,21,28 --wd ../../data/tests/ > log.txt 2>&1 &
-
-#Running sourmash for proteins 
-nohup python3 dnds_using_containment_index.py --ref_fasta ../../data/uniprotkb.fasta --query_fasta ../../data/500_sequences.fna --predict frame --k 7,14,21,28,35,42,49,56,63,70 --wd ../../data/ > log.txt 2>&1 &
