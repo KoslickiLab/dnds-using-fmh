159bfbdb9089cf218028eba8a3d34bdfc08ea9a6
Judith Rodriguez <judiths.r91@gmail.com>
2023-02-09 12:53:24 -0500

    organization of scripts and files

--- src/sourmash_dnds_estimation/step1_sourmash_compare_prot.sh
+++ src/sourmash_dnds_estimation/step1_sourmash_compare_prot.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 set -eoux pipefail
 
-#files for a simple sequence
-wd=/data/jzr5814/data/tests/prot/simple/
-#files for a single sequence
-#wd=/data/jzr5814/data/tests/prot/single_seq/
+### Sketch and comapre protein sequences for dN/dS estimation
+
+#working directories for data and result output
+data=/data/jzr5814/sourmash_dnds_estimation/tests/data/ #.faa and .fna files are found
+sigs=/data/jzr5814/sourmash_dnds_estimation/tests/data/signatures/prot/ #sketch sig output directory
+wd=/data/jzr5814/sourmash_dnds_estimation/tests/results/sourmash_compare/sourmash_compare_protein/ #output for sourmash comapre directory
+
 #files for a 9 sequences
-#wd=/data/jzr5814/data/tests/prot/9_seqs/
-ref=${wd}ref.fna
-samples=${wd}query.fna 
-ref_output=${wd}ref.sig 
-samp_output=${wd}query.sig.zip
-ref_scaled=1
-samp_scaled=1
-#K=7
+ref=${data}ref.fna
+samples=${data}query.fna
+
+#signature output filenames
+ref_output=${sigs}ref.sig 
+samp_output=${sigs}query.sig.zip
+
+#other parameters
+ref_scaled=1 #scale factor for reference
+samp_scaled=1 #scale factor for query
 
 #Input are protein sequencess
 #sourmash sketch protein -p k=7,k=14,k=21,k=28,k=35,k=42,k=49,k=56,k=63,k=70,scaled=$ref_scaled $ref -o $ref_output
@@ -23,9 +28,6 @@ samp_scaled=1
 sourmash sketch translate -p k=5,k=10,k=15,k=20,scaled=$ref_scaled $ref -o $ref_output
 sourmash sketch translate -p k=5,k=10,k=15,k=20,scaled=$samp_scaled $samples --singleton -o $samp_output
 
-#nohup sourmash compare $ref_output $samp_output --containment --protein --o ${wd}compare${K}.mat --csv ${wd}compare${K}.csv  --ksize $K > ${wd}compare${K}.txt 2>&1 &
-
-#for K in 7 14 21 28 35 42 49 56 62 70 
 for K in 5 10 15 20 
 do
 
---
+++ src/sourmash_dnds_estimation/step2_estimate_dnds.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eoux pipefail
+
+#working directories for data and result output
+nt_compare_wd='/data/jzr5814/sourmash_dnds_estimation/tests/results/sourmash_compare/sourmash_compare_nt/' #DNA compare
+prot_compare_wd='/data/jzr5814/sourmash_dnds_estimation/tests/results/sourmash_compare/sourmash_compare_protein/' #protein/translate compare
+wd='/data/jzr5814/sourmash_dnds_estimation/tests/results/DNDS/' #output dNdS results to this directory
+
+for k in 5 10 15 20
+do
+
+nt_containment=${nt_compare_wd}'compare'${k}'.csv'
+protein_containment=${prot_compare_wd}'compare'${k}'.csv'
+
+out=${wd}'dNdS_'${k}.csv
+
+nohup python3 /data/jzr5814/dnds-using-fmh/src_jzr/dnds_CI.py --nt ${nt_containment} --protein ${protein_containment} --k ${k} --o ${out} > ${wd}dnds_CI_log.txt 2>&1 &
+
+done
