b87cf6c6d44e498a9ea097aaad31efd0166d811b
Judith Rodriguez <judiths.r91@gmail.com>
2023-02-09 11:31:50 -0500

    remove unused functions

---
+++ src/sourmash_dnds_estimation/dNdS/reportCI.py
@@ -0,0 +1,17 @@
+import pickle
+import pandas as pd
+
+def grab_containment_from_mat(mat_df,ksize):
+    """This function converts matrix into df removes pairwise information"""
+    """When running contained_by() from sourmash api, a matrix via a csv file called compare.csv is produced"""
+    mat = pd.read_csv(mat_df,sep=',')
+    df = pd.DataFrame(columns=['A','B','containment','ksize'],dtype=int)
+    for i in range(len(mat)):
+        for j in range(len(mat)):
+            if i > j:
+                containment = mat.iloc[j,i]
+                df = pd.concat([df, pd.DataFrame.from_records([{'A':mat.columns[i],
+                'B':mat.columns[j],
+                'containment':containment,
+                'ksize':ksize}])],join='inner')
+    return(df)
