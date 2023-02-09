9302c12886e1e381773c60adcae28477177ca86d
Judith Rodriguez <judiths.r91@gmail.com>
2023-02-09 11:34:59 -0500

    modify detail of function

--- src/sourmash_dnds_estimation/dNdS/reportCI.py
+++ src/sourmash_dnds_estimation/dNdS/reportCI.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def grab_containment_from_mat(mat_df,ksize):
     """This function converts matrix into df removes pairwise information"""
-    """When running contained_by() from sourmash api, a matrix via a csv file called compare.csv is produced"""
+    """When running sourmash comapre, a matrix via a csv file called compare.csv is produced"""
     mat = pd.read_csv(mat_df,sep=',')
     df = pd.DataFrame(columns=['A','B','containment','ksize'],dtype=int)
     for i in range(len(mat)):
