f368db6dc8dcdd845c597aba2f76a5f37c6d9618
Judith Rodriguez <judiths.r91@gmail.com>
2023-02-28 14:25:30 -0500

    updated analyses

--- src/sourmash_dnds_estimation/step1_sourmash_compare_nt.sh
+++ src/sourmash_dnds_estimation/step1_sourmash_compare_nt.sh
@@ -28,13 +28,13 @@ samp_output=${sigs}ground_truth_mutated_queries_dna.sig.zip
 ref_scaled=1 #scale factor for reference
 samp_scaled=1 #scale factor for query
 
-sourmash sketch dna -p k=5,k=10,k=15,k=20,scaled=$ref_scaled $ref -o $ref_output
+sourmash sketch dna -p k=2,k=3,k=4,k=5,k=6,k=7,k=8,k=9,k=10,k=11,k=12,k=13,k=14,k=15,k=20,scaled=$ref_scaled $ref -o $ref_output
 
-sourmash sketch dna -p k=5,k=10,k=15,k=20,scaled=$samp_scaled $samples --singleton -o $samp_output
+sourmash sketch dna -p k=2,k=3,k=4,k=5,k=6,k=7,k=8,k=9,k=10,k=11,k=12,k=13,k=14,k=15,k=20,scaled=$samp_scaled $samples --singleton -o $samp_output
 
 #nohup sourmash compare $ref_output $samp_output --containment --dna --o ${wd}compare${K}.mat --csv ${wd}compare${K}.csv  --ksize $K > ${wd}compare${K}.txt 2>&1 &
 
-for K in 2 3 4 5 10 15 20 
+for K in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 20 
 do
     nohup sourmash compare $ref_output $samp_output --containment --dna --o ${wd}compare${K}.mat --csv ${wd}compare${K}.csv  --ksize $K > ${wd}compare${K}.txt 2>&1 & #assign to diff log files
 
