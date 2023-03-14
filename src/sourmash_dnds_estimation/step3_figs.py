7be02650cc24c9b52a2b4c22693000cf4c731a28
Judith Rodriguez <judiths.r91@gmail.com>
2023-03-14 12:41:25 -0400

    modified

--- src/sourmash_dnds_estimation/step3_figs.py
+++ src/sourmash_dnds_estimation/step3_figs.py
@@ -10,26 +10,34 @@ def scatter(ground_truth_dnds_results_df,sourmash_compare_dnds_results_df,ksize,
     expected_dnds_results_df = pd.read_csv(ground_truth_dnds_results_df)
     sourmash_compare_dnds_results_df = pd.read_csv(sourmash_compare_dnds_results_df)
 
-    results = pd.concat([sourmash_compare_dnds_results_df['dNdS_ratio'],expected_dnds_results_df['dNdS_ground_truth']], axis=1).reindex(expected_dnds_results_df.index)
+    #results = pd.concat([sourmash_compare_dnds_results_df['dNdS_ratio'],expected_dnds_results_df['dNdS_ground_truth']], axis=1).reindex(expected_dnds_results_df.index)
     
     plt.clf()
-    plt.scatter(results['dNdS_ground_truth'],results['dNdS_ratio'])
-    #tmp = [min(expected_dnds_results_df['dNdS_ground_truth']), max(expected_dnds_results_df['dNdS_ground_truth'])]
-    #tmp = [0,0.5]
-    #plt.xlim(0,0.5)
-    #plt.plot(tmp, tmp, linestyle='--')
-    plt.title(f'Expected dNdS vs. Containment dNdS using ksize={ksize}')
-    plt.ylabel('containment_dNdS')
-    plt.xlabel('expected_dNdS')
-    plt.savefig(f'{wd}scatter{ksize}.png',bbox_inches='tight')
+    #plt.scatter(results['dNdS_ground_truth'],results['dNdS_ratio'])
+    plt.scatter(expected_dnds_results_df['approx_dnds_using_fmh'],sourmash_compare_dnds_results_df['dNdS_ratio'])
+    tmp = [min(expected_dnds_results_df['approx_dnds_using_fmh']), max(expected_dnds_results_df['approx_dnds_using_fmh'])]
+    plt.plot(tmp, tmp, linestyle='--')
+    #plt.ylim(0,20)
+    plt.ylim(0,5)
+    plt.xlim(0,5)
+    plt.title(f'10,000 nt sequence p=0.1 ksize={ksize} scaled=1')
+    #plt.title(f'real protein-coding sequence p=0.1 ksize={ksize}')
+    #plt.ylabel('smash_containment_dNdS')
+    plt.ylabel('jzr_smash_cfrac_dnds')
+    plt.xlabel('mahmudur_smash_fmh_dnds')
+    #plt.savefig(f'{wd}scatter{ksize}.png',bbox_inches='tight')
+    plt.savefig(f'{wd}jzr_cfrac_vs_mahmudur_smash_fmh_dnds{ksize}.png',bbox_inches='tight')
 
 #for k in [2,3,4,5,6,7,8,9,10,11,12,13,14,15]:
-for k in [5,6,7,8,9,10]:
-#for k in 7:
+#for k in [5,6,7,8,9,10]:
+for k in [7]:
     WD='/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/'
-    ground_truth='/data/jzr5814/sourmash_dnds_estimation/tests/data/ground_truth_data/dNdS_ground_truth.csv'
-    sourmash_compare_dnds=f'/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/dNdS_{k}.csv'
-    scatter(ground_truth_dnds_results_df=ground_truth,sourmash_compare_dnds_results_df=sourmash_compare_dnds,ksize=k,wd=WD)
+    ground_truth=f'{WD}dNdS_ground_truth.csv'
+    sourmash_compare_dnds=f'{WD}dNdS_7.csv'
+    sourmash_compare_dnds_modified_file=f'{WD}dNdS_7_modified_removed_line_0.csv'
+    fmh_dnds=f'{WD}fmh_dnds.txt'
+    smash_fmh_dnds=f'{WD}smash_fmh_dnds.txt'
+    scatter(ground_truth_dnds_results_df=smash_fmh_dnds,sourmash_compare_dnds_results_df=sourmash_compare_dnds,ksize=k,wd=WD)
 
 """
 def multiplot_scatter(ground_truth_dnds_results_df,sourmash_compare_dnds_results_df,ksize):
