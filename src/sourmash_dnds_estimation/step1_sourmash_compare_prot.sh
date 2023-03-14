97cb70e3712a650c7d6b9a60cad7ffc487e627ea
Judith Rodriguez <judiths.r91@gmail.com>
2023-03-14 18:00:17 -0400

    modified sourmash sketch translate to sourmash sketch protein

--- src/sourmash_dnds_estimation/step1_sourmash_compare_prot.sh
+++ src/sourmash_dnds_estimation/step1_sourmash_compare_prot.sh
@@ -19,16 +19,16 @@ mkdir ${wd}
 #samples=${data}query.fna
 #ref=${data}ground_truth_ref_used_for_dNdS.fna
 #samples=${data}ground_truth_mutated_queries_used_for_dNdS.fna
-ref=${data}10000_nt_ref_seq.fna
-samples=${data}10000_nt_mutated_queries_seq.fna
+ref=${data}10000_prot_ref_seq.fna
+samples=${data}10000_prot_mutated_queries_seq.fna
 
 #signature output filenames
 #ref_output=${sigs}ref.sig 
 #samp_output=${sigs}query.sig.zip
 #ref_output=${sigs}ground_truth_ref_translate.sig 
 #samp_output=${sigs}ground_truth_mutated_queries_translate.sig.zip
-ref_output=${sigs}10000_nt_ref_seq_translate.sig 
-samp_output=${sigs}10000_nt_ref_seq_translate.sig.zip
+ref_output=${sigs}10000_prot_ref_seq.sig 
+samp_output=${sigs}10000_prot_mutated_queries_seq.sig.zip
 
 #other parameters
 ref_scaled=1 #scale factor for reference
@@ -39,10 +39,10 @@ samp_scaled=1 #scale factor for query
 #sourmash sketch protein -p k=7,k=14,k=21,k=28,k=35,k=42,k=49,k=56,k=63,k=70,scaled=$samp_scaled $samples --singleton -o $samp_output
 
 #Input are DNA sequences
-sourmash sketch translate -p k=7,scaled=$ref_scaled $ref -o $ref_output
+sourmash sketch protein -p k=7,scaled=$ref_scaled $ref -o $ref_output
 #sourmash sketch translate -p k=2,k=3,k=4,k=5,k=6,k=7,k=8,k=9,k=10,k=11,k=12,k=13,k=14,k=15,k=20,scaled=$ref_scaled $ref -o $ref_output
 
-sourmash sketch translate -p k=7,scaled=$samp_scaled $samples --singleton -o $samp_output
+sourmash sketch protein -p k=7,scaled=$samp_scaled $samples --singleton -o $samp_output
 #sourmash sketch translate -p k=2,k=3,k=4,k=5,k=6,k=7,k=8,k=9,k=10,k=11,k=12,k=13,k=14,k=15,k=20,scaled=$samp_scaled $samples --singleton -o $samp_output
 
 #for K in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 20
