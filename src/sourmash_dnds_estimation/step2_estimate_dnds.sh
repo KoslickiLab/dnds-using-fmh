22da27b677e234f6c47997b2bfc7c7d55b7857bd
Judith Rodriguez <judiths.r91@gmail.com>
2023-03-16 13:40:23 -0400

    code updated

--- src/sourmash_dnds_estimation/step2_estimate_dnds.sh
+++ src/sourmash_dnds_estimation/step2_estimate_dnds.sh
@@ -2,18 +2,19 @@
 set -eoux pipefail
 
 #working directories for data and result output
-nt_compare_wd='/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/sourmash_compare_nt/' #DNA compare
-prot_compare_wd='/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/sourmash_compare_protein/' #protein/translate compare
-wd='/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/' #output dNdS results to this directory
+wd=/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/real_data_0.1_ksizes_5_10/ #output dNdS results to this directory
+nt_compare_wd=${wd}sourmash_compare_nt/ #DNA compare
+prot_compare_wd=${wd}/sourmash_compare_protein/ #protein/translate compare
 
-#for k in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 20
-for k in 7
+for k in 5 6 7 8 9 10
 do
-nt_containment=${nt_compare_wd}'compare21.csv'
-protein_containment=${prot_compare_wd}'compare'${k}'.csv'
+declare -i nt_k=k*3
 
-out=${wd}'dNdS_'${k}.csv
+nt_containment=${nt_compare_wd}compare${nt_k}.csv
+protein_containment=${prot_compare_wd}compare${k}.csv
 
-nohup python3 dnds_CI.py --nt ${nt_containment} --protein ${protein_containment} --k ${k} --o ${out} > ${wd}dnds_CI_log.txt 2>&1 &
+out=${wd}dNdS_${k}.csv
+
+nohup python3 dnds_CI.py --nt ${nt_containment} --protein ${protein_containment} --k ${k} --o ${out} --wd ${wd} > ${wd}dnds_CI_log.txt 2>&1 &
 
 done
