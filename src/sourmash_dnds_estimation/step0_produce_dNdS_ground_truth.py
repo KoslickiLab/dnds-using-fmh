ce30085fdce4bb02b6efa45e7f1ed194f5af7f4d
Judith Rodriguez <judiths.r91@gmail.com>
2023-03-16 13:20:36 -0400

    added working directory flag to code and modified how start and codon are ignored

--- src/sourmash_dnds_estimation/step0_produce_dNdS_ground_truth.py
+++ src/sourmash_dnds_estimation/step0_produce_dNdS_ground_truth.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import argparse
 from dNdS import create_ground_truth_file
+from Bio.Seq import Seq
 
 """CFrac dN/dS estimator is tested with a ground truth file. This program creates a ground truth file that has\
 three columns that include the refernce sequence name, mutation rate p, and dNdS value based on the mutation rate p"""
@@ -11,8 +12,11 @@ def main(args):
     GROUND_TRUTH = args.ground_truth_output
     GROUND_TRUTH_QUERIES = open(args.ground_truth_queries_output,'w')
     GROUND_TRUTH_REF_OUTPUT = open(args.ground_truth_ref_output,'w')
-    mutation_p_list = args.mutation_rate_p.split(',')
+    mutation_p = args.mutation_rate_p
     ITERATIONS = args.iterations
+
+    GROUND_TRUTH_REF_PROT_OUTPUT = open('/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/translated_ref_seq.faa','w')
+    GROUND_TRUTH_PROT_QUERIES = open('/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/translated_mutated_queries_seq.faa','w')
 
     dNdS_report = []
 
@@ -29,45 +33,54 @@ def main(args):
             #next funcstion needs ref to be the same size, we ignore the start and stop codons and capitalize all characters        
             ref_seq = line.strip()[3:-3].upper()
 
-            for p in mutation_p_list:
+            for i in range(ITERATIONS):
 
-                for i in range(ITERATIONS):
+                #ref sequence is mutated with mutation rate p
+                query_nt_seq = create_ground_truth_file.mutated_sequence_based_on_mutation_rate_p(line.strip(),float(p))
 
-                    #ref sequence is mutated with mutation rate p
-                    query_nt_seq = create_ground_truth_file.mutated_sequence_based_on_mutation_rate_p(line.strip(),float(p))
+                GROUND_TRUTH_QUERIES.write(f'>gene_{p}_{i}\n')
+                GROUND_TRUTH_QUERIES.write(f'{query_nt_seq}\n') 
 
-                    GROUND_TRUTH_QUERIES.write(f'>gene_{p}_{i}\n')
-                    GROUND_TRUTH_QUERIES.write(f'{query_nt_seq}\n') 
-                    
-                    #get total numver of nucleotide mutations
-                    total_nt_mutations = create_ground_truth_file.total_nucleotide_mutations(ref_seq,query_nt_seq) 
+                #Translated mutated sequences 
+                translated_queries = Seq(query_nt_seq[3:-3]).translate()
+                GROUND_TRUTH_PROT_QUERIES.write(f'>gene_{p}_{i}\n')
+                GROUND_TRUTH_PROT_QUERIES.write(f'{translated_queries}\n')
+                            
+                #get total numver of nucleotide mutations
+                total_nt_mutations = create_ground_truth_file.total_nucleotide_mutations(ref_seq,query_nt_seq) 
 
-                    #get total number of nonsynonymous mutations
-                    total_nonsyn_mutations = create_ground_truth_file.total_aa_differences(ref_seq,query_nt_seq)
-                    dN = total_nonsyn_mutations/(len(ref_seq)/3)
+                #get total number of nonsynonymous mutations
+                total_nonsyn_mutations = create_ground_truth_file.total_aa_differences(ref_seq,query_nt_seq)
+                dN = total_nonsyn_mutations/(len(ref_seq)/3)
 
-                    #get total number of synonymous mutations
-                    total_syn_mutations = create_ground_truth_file.total_synonymous_mutations(total_nt_mutations,total_nonsyn_mutations)
-                    dS = total_syn_mutations/(len(ref_seq)/3)
+                #get total number of synonymous mutations
+                total_syn_mutations = create_ground_truth_file.total_synonymous_mutations(total_nt_mutations,total_nonsyn_mutations)
+                dS = total_syn_mutations/(len(ref_seq)/3)
 
-                    #estimate dNdS using Koslicki's suggestiion
-                    dNdS = create_ground_truth_file.koslicki_dnds(total_nonsyn_mutations,total_syn_mutations)
+                #estimate dNdS using Koslicki's suggestiion
+                dNdS = create_ground_truth_file.koslicki_dnds(total_nonsyn_mutations,total_syn_mutations)
 
-                    #selection
-                    if dNdS is None:
-                        selection='undetermined'
-                    elif dNdS == 1:
-                        selection='neutral'
-                    elif dNdS > 1:
-                        selection='positive/constrained'
-                    elif dNdS < 1:
-                        selection='negative'
+                #selection
+                if dNdS is None:
+                    selection='undetermined'
+                elif dNdS == 1:
+                    selection='neutral'
+                elif dNdS > 1:
+                    selection='positive/constrained'
+                elif dNdS < 1:
+                    selection='negative'
 
-                    #save report in a list object to later convert into a dataframe
-                    dNdS_report.append([f'ref_{p}_{i}', p, dN, dS, dNdS, selection, i])
+                #save report in a list object to later convert into a dataframe
+                dNdS_report.append([f'ref_{p}_{i}', p, dN, dS, dNdS, selection, i])
 
             GROUND_TRUTH_REF_OUTPUT.write(f'>ref_gene\n')
             GROUND_TRUTH_REF_OUTPUT.write(ref_seq+"\n")
+
+            #save translated ref sequence to the following files
+            ref_seq_translated = Seq(ref_seq).translate()
+            GROUND_TRUTH_REF_PROT_OUTPUT.write(f'>ref_gene\n')
+            GROUND_TRUTH_REF_PROT_OUTPUT.write(str(ref_seq_translated)+"\n")
+
 
     #save report to csv output file
     pd.DataFrame(dNdS_report, columns=['mutated_sequence', 'mutation_rate_p','dN','dS','dNdS_ground_truth','selection','iteration']).to_csv(GROUND_TRUTH)
@@ -104,15 +117,22 @@ if __name__ == "__main__":
 
     parser.add_argument(
         '--mutation_rate_p',
-        default='0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1',
-        type=str,
-        help = 'provide a list of mutation rate p values to mutate reference sequences to obtain ground truth query mutated sequence'
+        default=0.1,
+        type=int,
+        help = 'provide a mutation rate p values to mutate reference sequences to obtain ground truth query mutated sequence'
     )
 
     parser.add_argument(
         '--iterations',
         type=int,
         help = 'provide a list of mutation rate p values to mutate reference sequences to obtain ground truth query mutated sequence'
+    )
+
+    parser.add_argument(
+        '--wd',
+        type=str,
+        default='',
+        help = 'provide working directory to save all output'
     )
 
     args = parser.parse_args()
