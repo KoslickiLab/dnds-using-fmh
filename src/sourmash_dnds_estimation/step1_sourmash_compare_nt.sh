e5d2fb18dab38c865db9e6d21c8cad2f89b88195
Judith Rodriguez <judiths.r91@gmail.com>
2023-04-06 14:15:07 -0400

    modifications for analyses

--- src/sourmash_dnds_estimation/step1_sourmash_compare_nt.sh
+++ src/sourmash_dnds_estimation/step1_sourmash_compare_nt.sh
@@ -4,18 +4,19 @@ set -eoux pipefail
 ### Sketch and comapre nucleotide sequences for dN/dS estimation
 
 #working directories for data and result output
-data=/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/real_data_0.1_ksizes_5_30_500_cdn_differences/
+data=/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/real_data_ksizes_5_30_K03427_1452nt/
+#data=$1
 sigs=${data}
 wd=${data}sourmash_compare_nt/
 
 mkdir ${wd}
 #fasta files
-ref=${data}ground_truth_ref_used_for_dNdS.fna
-samples=${data}ground_truth_mutated_queries_used_for_dNdS.fna
+#ref=${data}ground_truth_ref_used_for_dNdS.fna
+#samples=${data}ground_truth_mutated_queries_used_for_dNdS.fna
 #ref=${data}2499_nt_ref_seq.fna
 #samples=${data}2499_nt_mutated_queries_seq.fna
-#ref=${data}K03427_1452nt_ref_seq.fna
-#samples=${data}K03427_1452nt_query_seqs.fna
+ref=${data}K03427_1452nt_ref_seq.fna
+samples=${data}K03427_1452nt_query_seqs.fna
 
 #signature output filenames
 ref_output=${sigs}ground_truth_ref_dna.sig 
@@ -29,13 +30,13 @@ samp_scaled=1 #scale factor for query
 
 #k is k*3 for nt when comparing between protein and nt
 #sourmash sketch dna -p k=21,scaled=$ref_scaled $ref -o $ref_output
-sourmash sketch dna -p k=15,k=18,k=21,k=24,k=27,k=30,k=45,k=60,k=75,k=90,scaled=$ref_scaled $ref -o $ref_output
+sourmash sketch dna -p k=15,k=18,k=21,k=24,k=27,k=30,k=33,k=36,k=39,k=42,k=45,k=60,k=75,k=90,scaled=$ref_scaled $ref -o $ref_output
 
 #sourmash sketch dna -p k=21,scaled=$samp_scaled $samples --singleton -o $samp_output
-sourmash sketch dna -p k=15,k=18,k=21,k=24,k=27,k=30,k=45,k=60,k=75,k=90,scaled=$samp_scaled $samples --singleton -o $samp_output
+sourmash sketch dna -p k=15,k=18,k=21,k=24,k=27,k=30,k=33,k=36,k=39,k=42,k=45,k=60,k=75,k=90,scaled=$samp_scaled $samples --singleton -o $samp_output
 
 
-for K in 15 18 21 24 27 30 45 60 75 90
+for K in 15 18 21 24 27 30 33 36 39 42 45 60 75 90
 do
     nohup sourmash compare $ref_output $samp_output --containment --dna --o ${wd}compare${K}.mat --csv ${wd}compare${K}.csv  --ksize $K > ${wd}compare${K}.txt 2>&1 & #assign to diff log files
 
