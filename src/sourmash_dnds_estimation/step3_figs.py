c1156ef60fcd5ed2eed77746833f0fd1cc1961a9
Judith Rodriguez <judiths.r91@gmail.com>
2023-04-10 15:26:35 -0400

    threshold value

--- src/sourmash_dnds_estimation/step3_figs.py
+++ src/sourmash_dnds_estimation/step3_figs.py
@@ -10,8 +10,8 @@ import math
 
 def scatter(ground_truth_dnds_results_df,sourmash_compare_dnds_results_df,ksize,wd):
 
-    str_len=10002
-    p=0.1
+    str_len=1000
+    p=0.001
 
     expected_dnds_results_df = pd.read_csv(ground_truth_dnds_results_df)
     sourmash_compare_dnds_results_df = pd.read_csv(sourmash_compare_dnds_results_df)
@@ -20,13 +20,16 @@ def scatter(ground_truth_dnds_results_df,sourmash_compare_dnds_results_df,ksize,
     column_sourmash_compare_dnds_results_df = 'dNdS_ratio'
     column_expected_dnds_results_df = 'dNdS_ground_truth'
 
-    results = pd.concat([sourmash_compare_dnds_results_df[column_sourmash_compare_dnds_results_df],expected_dnds_results_df[column_expected_dnds_results_df]], axis=1).reindex(expected_dnds_results_df.index)
+    #results = pd.concat([sourmash_compare_dnds_results_df[column_sourmash_compare_dnds_results_df],expected_dnds_results_df[column_expected_dnds_results_df]], axis=1).reindex(expected_dnds_results_df.index)
+    results = pd.concat([sourmash_compare_dnds_results_df,expected_dnds_results_df], axis=1).reindex(expected_dnds_results_df.index)
     
     #filter 
     results_2 = results.replace([np.inf, -np.inf], np.nan).dropna() #remove indivisible dNdS ratio
     results_2 = results_2[results_2['containment_nt']!=results_2['containment_protein']] #remove equal nt and protein containments, this gives HUGE number
     results_2 = results_2[results_2['containment_nt']<=results_2['containment_protein']] #remove if nt containment is greater than protein containment, this gives negative dnds
-    results_2 = results_2[(results_2['containment_protein']/results_2['containment_nt'])<=1] #remove if they are close by a magnitud of 1
+    #results_2 = results_2[(results_2['containment_protein']/results_2['containment_nt'])>=1.0] #remove if they are close by a magnitud of 1
+    results_2 = results_2[(results_2['containment_protein']/results_2['containment_nt'])>=1.00002] #remove if they are close by a magnitud of 1
+    results_2 = results_2[[column_sourmash_compare_dnds_results_df,column_expected_dnds_results_df]]
     #print(results_2)
     #print(results_2.query('correct_dnds != dNdS_ground_truth'))
 
@@ -38,15 +41,15 @@ def scatter(ground_truth_dnds_results_df,sourmash_compare_dnds_results_df,ksize,
     plt.clf()
     plt.scatter(results[column_expected_dnds_results_df],results[column_sourmash_compare_dnds_results_df])
 
-    #plt.text(0,25,f'RMSE={round(RMSE,4)}')
-    #plt.text(0,24,f'pearson r={round(corr[0],4)},p={round(corr[1])}')
+    plt.text(0,28,f'RMSE={round(RMSE,4)}')
+    plt.text(0,26,f'pearson r={round(corr[0],4)},p={round(corr[1])}')
 
-    plt.text(2,4,f'RMSE={RMSE}')
-    plt.text(2,3.9,f'pearson r={corr[0]},p={corr[1]}')
+    #plt.text(2.1,28,f'RMSE={RMSE}')
+    #plt.text(2.1,26,f'pearson r={corr[0]},p={corr[1]}')
 
     tmp = [results[column_expected_dnds_results_df].min(), results[column_expected_dnds_results_df].max()]
     plt.plot(tmp, tmp, linestyle='--')
-    #plt.ylim(-5,30)
+    plt.ylim(-5,30)
     
     #plt.title(f'10,000 nt sequence p=0.1 ksize={ksize} scaled=1')
     plt.title(f'dN/dS real protein-coding sequence scaled=1 p={p} ksize={ksize} len(nt)={str_len}')
@@ -56,8 +59,8 @@ def scatter(ground_truth_dnds_results_df,sourmash_compare_dnds_results_df,ksize,
     plt.ylabel('smash_containment_dNdS\n outliers removed')
     #plt.ylabel('mahmudur ground truth dN/dD')
 
-    plt.xlabel('jzr ground truth dN/dS\n(counts total nt differences)')
-    #plt.xlabel('jzr ground truth dN/dS\n(counts total codon differences)') 
+    #plt.xlabel('jzr ground truth dN/dS\n(counts total nt differences)')
+    plt.xlabel('jzr ground truth dN/dS\n(counts total codon differences)') 
 
     #plt.savefig(f'{wd}jzr_ground_truth_vs_mahmudur_ground_truth_{p}_{ksize}_{str_len}.png',bbox_inches='tight')
     #plt.savefig(f'{wd}smash_dnds_and_ground_truth_dnds_{p}_{ksize}_{str_len}.png',bbox_inches='tight')
@@ -66,7 +69,7 @@ def scatter(ground_truth_dnds_results_df,sourmash_compare_dnds_results_df,ksize,
 for k in [5,6,7,8,9,10,11,12,13,14,15,20,25,30]:
 #for k in [5,6,7,8,9,10,15]:
 #for k in [25,30]:
-    WD='/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/real_data_0.1_ksizes_5_30_10002/'
+    WD='/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/real_data_0.001_ksizes_5_30_1000_cdn/'
     ground_truth=f'{WD}dNdS_ground_truth.csv'
 
     sourmash_compare_dnds=f'{WD}dnds_{k}.csv'
