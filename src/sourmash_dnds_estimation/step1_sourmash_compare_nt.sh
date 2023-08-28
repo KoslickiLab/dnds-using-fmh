d4df5d589c5cdc5cffccee3da5f855a17340ee9e
Judith Rodriguez <judiths.r91@gmail.com>
2023-08-28 14:35:33 -0400

    .

--- src/sourmash_dnds_estimation/step1_sourmash_compare_nt.sh
+++ src/sourmash_dnds_estimation/step1_sourmash_compare_nt.sh
@@ -4,41 +4,33 @@ set -eoux pipefail
 ### Sketch and comapre nucleotide sequences for dN/dS estimation
 
 #working directories for data and result output
-data=/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/real_data_ksizes_5_30_K03427_1452nt/
+data=/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/HIT000324409/
 #data=$1
 sigs=${data}
 wd=${data}sourmash_compare_nt/
 
 mkdir ${wd}
 #fasta files
-#ref=${data}ground_truth_ref_used_for_dNdS.fna
-#samples=${data}ground_truth_mutated_queries_used_for_dNdS.fna
-#ref=${data}2499_nt_ref_seq.fna
-#samples=${data}2499_nt_mutated_queries_seq.fna
-ref=${data}K03427_1452nt_ref_seq.fna
-samples=${data}K03427_1452nt_query_seqs.fna
+ref=${data}ref_HIT000324409.fastn
+samples=${data}queries_HIT000324409.fastn
 
 #signature output filenames
-ref_output=${sigs}ground_truth_ref_dna.sig 
-samp_output=${sigs}ground_truth_mutated_queries_dna.sig.zip
-#ref_output=${sigs}2499_nt_ref_seq.sig 
-#samp_output=${sigs}2499_nt_ref_seq.sig.zip
+ref_output=${sigs}ref_dna.sig 
+samp_output=${sigs}queries_dna.sig.zip
 
 #other parameters
 ref_scaled=1 #scale factor for reference
 samp_scaled=1 #scale factor for query
 
 #k is k*3 for nt when comparing between protein and nt
-#sourmash sketch dna -p k=21,scaled=$ref_scaled $ref -o $ref_output
 sourmash sketch dna -p k=15,k=18,k=21,k=24,k=27,k=30,k=33,k=36,k=39,k=42,k=45,k=60,k=75,k=90,scaled=$ref_scaled $ref -o $ref_output
-
-#sourmash sketch dna -p k=21,scaled=$samp_scaled $samples --singleton -o $samp_output
 sourmash sketch dna -p k=15,k=18,k=21,k=24,k=27,k=30,k=33,k=36,k=39,k=42,k=45,k=60,k=75,k=90,scaled=$samp_scaled $samples --singleton -o $samp_output
 
 
 for K in 15 18 21 24 27 30 33 36 39 42 45 60 75 90
 do
     nohup sourmash compare $ref_output $samp_output --containment --dna --o ${wd}compare${K}.mat --csv ${wd}compare${K}.csv  --ksize $K > ${wd}compare${K}.txt 2>&1 & #assign to diff log files
+    nohup sourmash compare $ref_output $samp_output --ani --dna --o ${wd}compare_ani${K}.mat --csv ${wd}compare_ani${K}.csv  --ksize $K > ${wd}compare_ani${K}.txt 2>&1 & #assign to diff log files
 
 done
 
--- src/sourmash_dnds_estimation/step2_estimate_dnds.sh
+++ src/sourmash_dnds_estimation/step2_estimate_dnds.sh
@@ -2,10 +2,9 @@
 set -eoux pipefail
 
 #working directories for data and result output
-wd=/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/real_data_ksizes_5_30_K03427_1452nt/ #output dNdS results to this directory
-#wd=$1
+wd=/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/HIT000324409/ #output dNdS results to this directory
 nt_compare_wd=${wd}sourmash_compare_nt/ #DNA compare
-prot_compare_wd=${wd}/sourmash_compare_protein/ #protein/translate compare
+prot_compare_wd=${wd}sourmash_compare_protein/ #protein/translate compare
 
 for k in 5 6 7 8 9 10 11 12 13 14 15 20 25 30
 do
@@ -14,7 +13,7 @@ declare -i nt_k=k*3
 nt_containment=${nt_compare_wd}compare${nt_k}.csv
 protein_containment=${prot_compare_wd}compare${k}.csv
 
-out=dnds_${k}.csv
+out=dnds_constant_${k}.csv
 
 nohup python3 dnds_CI.py --nt ${nt_containment} --protein ${protein_containment} --k ${k} --o ${out} --wd ${wd} > ${wd}dnds_CI_log.txt 2>&1 &
 
