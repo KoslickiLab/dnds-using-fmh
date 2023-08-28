91cf0a248c86026a63c11889fc2e8eea93a02bbc
Judith Rodriguez <judiths.r91@gmail.com>
2023-08-28 14:35:33 -0400

    .

--- src/kaks_calc_tool/compare_dN.py
+++ src/kaks_calc_tool/compare_dN.py
@@ -9,35 +9,32 @@ import numpy as np
 import math
 from sklearn.metrics import mean_absolute_error
 
-def scatter(ground_truth_data,fracminhash_data,ksize,wd,kaks_calc_method):
+def scatter(ground_truth_data,fracminhash_data,ksize,wd,gene_length,p_mut,kaks_calc_method):
 
     #fix kaks calculator dataframe by choosing method of interest, removing refvsref dnds and reindexing
     kaks_calc_data = pd.read_csv(ground_truth_data,delimiter='\t')
-    kaks_calc_data = kaks_calc_data[kaks_calc_data['Method']==kaks_calc_method].tail(-1).reset_index()[['Sequence','Ka/Ks']]
+    kaks_calc_data = kaks_calc_data[kaks_calc_data['Method']==kaks_calc_method].tail(-1).reset_index()[['Sequence','Ka']]
 
     #read in fracminhash dataframe
     fracminhash_data = pd.read_csv(fracminhash_data)
 
     #choose column names depending on the data
-    column_fracminhash_data = 'dNdS_ratio_constant' #USING SOURMASH
+    column_fracminhash_data = 'dN_constant' #USING SOURMASH
     
-    column_kaks_calc_data = 'Ka/Ks'
+    column_kaks_calc_data = 'Ka'
 
     results = pd.concat([fracminhash_data,kaks_calc_data], axis=1).reindex(kaks_calc_data.index)
     
     #filter 
     results_2 = results.replace([np.inf, -np.inf], np.nan).dropna() #remove indivisible dNdS ratio
     
-    
     #filter when using sourmash results
-    """
     results_2 = results_2[results_2['containment_nt']!=results_2['containment_protein']] #remove equal nt and protein containments, this gives HUGE number
     results_2 = results_2[results_2['containment_nt']<=results_2['containment_protein']] #remove if nt containment is greater than protein containment, this gives negative dnds
     Caa=results_2['containment_protein']
     Cnt=results_2['containment_nt']
     results_2 = results_2[Caa-Cnt>=0.001] #remove if too close for p mutation 0.001
-    """
-
+    
     #choosing columns of interest
     results_2 = results_2[[column_fracminhash_data,column_kaks_calc_data]]
     
@@ -60,38 +57,34 @@ def scatter(ground_truth_data,fracminhash_data,ksize,wd,kaks_calc_method):
  
     plt.clf()
     plt.scatter(results[column_kaks_calc_data],results[column_fracminhash_data])
-    
-    x_pos=0.0
     """
-    plt.text(x_pos,56,f'RMSE={round(RMSE,4)}')
-    plt.text(x_pos,53,f'MAE={round(MAE,4)}')
-    plt.text(x_pos,50,f'pearson r={round(pcorr[0],4)},p={round(pcorr[1])}')
-    plt.text(x_pos,47,f'R^2={round(R2,4)}')
-    plt.text(x_pos,44,f'n={n}')
+    x_pos=0.0004
+    plt.text(x_pos,0.0023,f'RMSE={round(RMSE,4)}')
+    plt.text(x_pos,0.0022,f'MAE={round(MAE,4)}')
+    plt.text(x_pos,0.0021,f'pearson r={round(pcorr[0],4)},p={round(pcorr[1])}')
+    plt.text(x_pos,0.0020,f'R^2={round(R2,4)}')
+    plt.text(x_pos,0.0019,f'n={n}')
     """
-    plt.text(x_pos,0.1,f'RMSE={round(RMSE,4)}')
-    plt.text(x_pos,0.08,f'MAE={round(MAE,4)}')
-    plt.text(x_pos,0.06,f'pearson r={round(pcorr[0],4)},p={round(pcorr[1])}')
-    plt.text(x_pos,0.04,f'R^2={round(R2,4)}')
-    plt.text(x_pos,0.02,f'n={n}')
-
     tmp = [results[column_kaks_calc_data].min(), results[column_kaks_calc_data].max()]
     plt.plot(tmp, tmp, linestyle='--')
-    #plt.ylim(0,5)
+    #plt.ylim(0,0.0025)
     
-    plt.title(f'dN/dS real protein-coding sequence scaled=1 ksize={ksize}')
-    plt.ylabel('fmh dN/dS\nsourmash containment')
-    plt.xlabel(f'{kaks_calc_method} dN/dS\nKa/Ks calculator')
+    plt.title(f'dN real protein-coding sequence scaled=1 p={p_mut} ksize={ksize} len(nt)={gene_length}')
+    plt.ylabel('fmh dN\nsourmash containment')
+    plt.xlabel(f'{kaks_calc_method} dN\nKa/Ks calculator')
 
-    plt.savefig(f'{wd}fmh_constant_and_{kaks_calc_method}_{ksize}_r2_MAE_RMSE_outliers_rm.png',bbox_inches='tight')
+    plt.savefig(f'{wd}Ka_fmh_constant_and_{kaks_calc_method}_{p_mut}_{ksize}_{gene_length}_r2_MAE_RMSE_outliers_rm.png',bbox_inches='tight')
 
 for k in [5,6,7,8,9,10,11,12,13,14,15,20,25,30]:
 
+    p_rate=0.1
+    length=10002
+    #KAKS_CALC_METHOD='GY-HKY'
     KAKS_CALC_METHOD='NG'
-    WD=f'/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/HIT000324409/'
+    WD=f'/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/real_data_{p_rate}_ksizes_5_30_{length}_cdn/'
     FRACMINHASH_DATA=f'{WD}dnds_constant_{k}.csv'
-    ka_ks_data = f'/data/jzr5814/kaks_calc_tool_analysis/HIT000324409/kaks_sequences.axt.kaks'
+    ka_ks_data = f'/data/jzr5814/kaks_calc_tool_analysis/real_data_{p_rate}_{length}_cdn/kaks_sequences.axt.kaks'
 
     scatter(ground_truth_data=ka_ks_data,fracminhash_data=FRACMINHASH_DATA,
-            ksize=k,wd=WD,
+            ksize=k,wd=WD,gene_length=length,p_mut=p_rate,
             kaks_calc_method=KAKS_CALC_METHOD)
--- src/sourmash_dnds_estimation/step1_sourmash_compare_prot.sh
+++ src/sourmash_dnds_estimation/step1_sourmash_compare_prot.sh
@@ -4,26 +4,19 @@ set -eoux pipefail
 ### Sketch and comapre protein sequences for dN/dS estimation
 
 #working directories for data and result output
-data=/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/real_data_ksizes_5_30_K03427_1452nt/
-#data=$1
+data=/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/HIT000324409/
 sigs=${data}
 wd=${data}sourmash_compare_protein/
 
 mkdir ${wd}
 
 #files for a 9 sequences
-ref=${data}translated_ref_seq.faa
-samples=${data}translated_mutated_queries_seq.faa
-#ref=${data}2499_prot_ref_seq.faa
-#samples=${data}2499_prot_mutated_queries_seq.faa
-#ref=${data}K03427_1452nt_ref_seq.fna
-#samples=${data}K03427_1452nt_query_seqs.fna
+ref=${data}ref_HIT000324409.fasta
+samples=${data}queries_HIT000324409.fasta
 
 #signature output filenames
-ref_output=${sigs}ground_truth_ref_translate.sig 
-samp_output=${sigs}ground_truth_mutated_queries_translate.sig.zip
-#ref_output=${sigs}2499_prot_ref_seq.sig 
-#samp_output=${sigs}2499_prot_mutated_queries_seq.sig.zip
+ref_output=${sigs}ref_translate.sig 
+samp_output=${sigs}queries_translate.sig.zip
 
 #other parameters
 ref_scaled=1 #scale factor for reference
@@ -31,7 +24,6 @@ samp_scaled=1 #scale factor for query
 
 #Input of ref protein sequencess 
 sourmash sketch protein -p k=5,k=6,k=7,k=8,k=9,k=10,k=11,k=12,k=13,k=14,k=15,k=20,k=25,k=30,scaled=$ref_scaled $ref -o $ref_output
-
 #Input of mutated protein sequencess
 sourmash sketch protein -p k=5,k=6,k=7,k=8,k=9,k=10,k=11,k=12,k=13,k=14,k=15,k=20,k=25,k=30,scaled=$samp_scaled $samples --singleton -o $samp_output
 
@@ -39,5 +31,6 @@ for K in 5 6 7 8 9 10 11 12 13 14 15 20 25 30
 do
 
     nohup sourmash compare $ref_output $samp_output --containment --protein --o ${wd}compare${K}.mat --csv ${wd}compare${K}.csv --ksize $K > ${wd}compare${K}.txt 2>&1 & #assign to diff log files
+    nohup sourmash compare $ref_output $samp_output --ani --protein --o ${wd}compare_ani${K}.mat --csv ${wd}compare_ani${K}.csv  --ksize $K > ${wd}compare_ani${K}.txt 2>&1 & #assign to diff log files
 
 done
