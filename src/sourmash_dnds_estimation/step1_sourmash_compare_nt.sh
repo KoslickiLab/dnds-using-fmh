5c2f472ac2f93709b8858c0d18baebe432f7b68c
Judith Rodriguez <judiths.r91@gmail.com>
2023-09-05 11:00:14 -0400

    changes to column A in grab_containment_from_mat_ground_truth

--- src/sourmash_dnds_estimation/step1_sourmash_compare_nt.sh
+++ src/sourmash_dnds_estimation/step1_sourmash_compare_nt.sh
@@ -4,15 +4,15 @@ set -eoux pipefail
 ### Sketch and comapre nucleotide sequences for dN/dS estimation
 
 #working directories for data and result output
-data=/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/HIT000324409/
+data=/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_ground_truth/HIT000324409_pairwise/
 #data=$1
 sigs=${data}
 wd=${data}sourmash_compare_nt/
 
 mkdir ${wd}
 #fasta files
-ref=${data}ref_HIT000324409.fastn
-samples=${data}queries_HIT000324409.fastn
+ref=${data}pairwise_HIT000324409.fastn
+samples=${data}pairwise_HIT000324409.fastn
 
 #signature output filenames
 ref_output=${sigs}ref_dna.sig 
@@ -23,7 +23,7 @@ ref_scaled=1 #scale factor for reference
 samp_scaled=1 #scale factor for query
 
 #k is k*3 for nt when comparing between protein and nt
-sourmash sketch dna -p k=15,k=18,k=21,k=24,k=27,k=30,k=33,k=36,k=39,k=42,k=45,k=60,k=75,k=90,scaled=$ref_scaled $ref -o $ref_output
+sourmash sketch dna -p k=15,k=18,k=21,k=24,k=27,k=30,k=33,k=36,k=39,k=42,k=45,k=60,k=75,k=90,scaled=$ref_scaled $ref --singleton -o $ref_output
 sourmash sketch dna -p k=15,k=18,k=21,k=24,k=27,k=30,k=33,k=36,k=39,k=42,k=45,k=60,k=75,k=90,scaled=$samp_scaled $samples --singleton -o $samp_output
 
 
