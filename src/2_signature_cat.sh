994d82bbb9437a07262baeb3e5973f87e8d4ede7
Judith Rodriguez <judiths.r91@gmail.com>
2023-12-12 18:47:25 -0500

    cleaning scripts

---
+++ src/2_signature_cat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+time -v
+set -eoux pipefail
+
+start=`date +%s`
+
+wd='/data/jzr5814/sourmash_dnds_estimation/tests/results/genomic_dnds/nakano_sequences'
+
+cd ${wd}
+mkdir signature_cat
+
+nohup sourmash signature cat ${wd}/signatures/*.dna.sig.gzip -o ${wd}/signature_cat/dna.cat.sig.gzip > ${wd}/cat_dna.log 2>&1 &
+nohup sourmash signature cat ${wd}/signatures/*.prot.sig.gzip -o ${wd}/signature_cat/prot.cat.sig.gzip > ${wd}/cat_protein.log 2>&1 &
+
