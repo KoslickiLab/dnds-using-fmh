b62cb6e387226a558534abfd978d918e589ad6d6
Judith Rodriguez <judiths.r91@gmail.com>
2024-02-06 16:48:13 -0500

    updating fmh_omega directory and files

--- src/fmh_omega/sourmash_api.py
+++ src/fmh_omega/sourmash_api.py
@@ -7,7 +7,6 @@ from pathlib import Path
 from loguru import logger
 
 """SKETCH FILES"""
-
 def sketch_dna(fasta, klist, scaled, out_sigfile):
     """sketch a DNA fasta file"""
     """fasta: Fasta input file"""
@@ -37,61 +36,31 @@ def sketch_protein(fasta, klist, scaled, out_sigfile):
         logger.error(f"Error occurred while sketching {fasta}: {e}")
 
 """CONCATENATE SIGNATURES"""
-
-def cat_signatures(signatures_dir, out_cat_sigfile):
+def cat_signatures(working_dir, molecule, out_name):
     """Concatenate signature files. This runs when a user has a list of genome fasta files"""
     """signature_list: list of signatures to concatenate"""
-    """outfile: signature name"""
-    cmd = f"mkdir signatures_dir/../signature_cat"
-    cmd = f"sourmash signature cat {signatures_dir}/* -o ${signatures_dir}/../signature_cat/{out_cat_sigfile}"
+    """molecule: only be dna or protein"""
+    cmd = f"sourmash signature cat {working_dir}/signatures/*.{molecule}.sig.gzip -o ${working_dir}/signatures/{out_name}.cat.{molecule}.sig.gzip"
     try:
-        logger.info(f"Making a new directory in ${signatures_dir}../ called signature_cat")
+        logger.info(f"Making a new directory in ${working_dir} called signature_cat")
         subprocess.run(cmd, shell=True, check=True)
-        logger.info(f"Concatenating individual genome signatures in: {signatures_dir}")
+        logger.info(f"Concatenating individual genome signatures in: {working_dir}/signatures")
         subprocess.run(cmd, shell=True, check=True)
         logger.success(f"Successfully concatenated")
     except subprocess.CalledProcessError as e:
-        logger.error(f"Error occurred while concatenating for signatures in {signatures_dir}: {e}")
+        logger.error(f"Error occurred while concatenating for signatures in {working_dir}/signatures: {e}")
 
 """COMPARE SIGNATURES VIA CONTAINMENT INDEX"""
-
-def create_compare_directory(dir,content):
-    """directory creation to store sourmash compare records"""
-    """dir: working directory"""
-    """content: identify whether it is dna or protein"""
-    cmd = f'mkdir {dir}/compare_{content}'
-    try:
-        logger.info(f"Creating compare_{content} in {dir}")
-        subprocess.run(cmd, shell=True, check=True)
-        logger.success(f"Successful make directory")
-    except subprocess.CalledProcessError as e:
-        logger.error(f"Error occurred while creating directory in {dir}")
-
-def compare_dna_signatures(ref_dna, query_dna, ksize, dir, out_compare_csv):
-    """compare dna signature file of ref and query."""
-    """ref_dna: reference dna signature"""
-    """query_dna: query dna signature"""
+def compare_signatures(ref, query, ksize, molecule, working_dir):
+    """compare dna or protein signature file of ref and query."""
+    """ref: reference dna or protein signature"""
+    """query: query dna or protein signature"""
     """outfile: signature name"""
-    cmd = f"sourmash compare {ref_dna} {query_dna} --containment --dna --ksize {ksize} --csv {dir}/{out_compare_csv}"
+    cmd = f"sourmash compare {ref} {query} --containment --{molecule} --ksize {ksize} --csv {working_dir}/compare_{molecule}/compare.{molecule}.${ksize}.csv"
     try:
-        logger.info(f"Comparing and obtaining containment index between {ref_dna} and {query_dna}")
+        logger.info(f"Comparing and obtaining containment index between {ref} and {query}")
         subprocess.run(cmd, shell=True, check=True)
         logger.success(f"Successfully compared")
     except subprocess.CalledProcessError as e:
-        logger.error(f"Error occurred while comparing {ref_dna} and {query_dna}: {e}")
-
-def compare_protein_signatures(ref_protein, query_protein, ksize, dir, out_compare_csv):
-    """compare dna signature file of ref and query."""
-    """ref_dna: reference dna signature"""
-    """query_dna: query dna signature"""
-    """outfile: signature name"""
-    cmd = f"sourmash compare {ref_protein} {query_protein} --containment --protein --ksize {ksize} --csv {dir}/{out_compare_csv}"
-    try:
-        logger.info(f"Comparing and obtaining containment index between {ref_protein} and {query_protein}")
-        subprocess.run(cmd, shell=True, check=True)
-        logger.success(f"Successfully compared")
-    except subprocess.CalledProcessError as e:
-        logger.error(f"Error occurred while comparing {ref_protein} and {query_protein}: {e}")
-
-
+        logger.error(f"Error occurred while comparing {ref} and {query}: {e}")
 
