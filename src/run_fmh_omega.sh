c3a4e6abfee087e73bf2a08e063c305af7444661
Judith Rodriguez <judiths.r91@gmail.com>
2024-02-07 20:52:14 -0500

    preparation for gKaKs paper data

--- src/run_fmh_omega.sh
+++ src/run_fmh_omega.sh
@@ -2,12 +2,12 @@
 set -eoux pipefail
 
 #Parameters
-wd=/data/jzr5814/sourmash_dnds_estimation/tests/test/testing_new_code_on_create_sequence_using_NG_assumption_0.01/positive
+wd=/data/jzr5814/sourmash_dnds_estimation/tests/results/genomic_dnds/gKaKs_paper
 klist='5,7'
 scaled=1
 cds_input_list=${wd}/fasta_files.txt
-out=testing_new_code_positive
-singles=yes
+out=gKaKs_paper
+singles=no
 
 #Run FMH Omega
 python3 fmh_omega_from_cds.py --cds_input ${cds_input_list} --scaled_input ${scaled} --klist ${klist} --singleton ${singles} --outname ${out} --working_dir ${wd}
