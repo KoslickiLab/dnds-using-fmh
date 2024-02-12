391dee90b1d84c0966d0a1bb42b5924e87acf0a6
Judith Rodriguez <judiths.r91@gmail.com>
2024-02-12 15:35:23 -0500

    update for genome-cds level analysis

--- src/run_fmh_omega.sh
+++ src/run_fmh_omega.sh
@@ -2,12 +2,12 @@
 set -eoux pipefail
 
 #Parameters
-wd=/data/jzr5814/repositories/dnds-using-fmh/src/demo/singleton
-klist='5,7'
+wd=/data/jzr5814/sourmash_dnds_estimation/tests/results/genomic_dnds/gKaKs_paper
+klist='5,7,9,11,13'
 scaled=1
 cds_input_list=${wd}/fasta_files.txt
-out=demo_singleton
-multiple=no #choose yes if you have multiple fastn files
+out=gKaKs_paper
+multiple=yes #choose yes if you have multiple fastn files
 
 #Run FMH Omega
 python3 script_fmh_omega.py --cds_input ${cds_input_list} --scaled_input ${scaled} --klist ${klist} --multiple ${multiple} --outname ${out} --working_dir ${wd}
