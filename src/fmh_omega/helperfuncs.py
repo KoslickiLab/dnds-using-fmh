4bfc0421d447c7b2edaa5ceb18434c6fd7cf5e59
Judith Rodriguez <judiths.r91@gmail.com>
2024-02-15 12:41:05 -0500

    added functions that create a selection based random sequence

--- src/fmh_omega/helperfuncs.py
+++ src/fmh_omega/helperfuncs.py
@@ -73,3 +73,99 @@ def return_signature_list(working_dir, molecule):
             sigs.append(file)
     return sigs
 
+#The following functions create a random sequence for selection
+
+def get_coding_sequence_from_nucleotide_sequence(nt_sequence):
+    """This functioin returns the coding sequence as a list when given a nucleotide sequence.
+    nt_sequence: nucleotide sequence, preferably a protein coding gene sequence"""
+    coding_sequence = list(sliced(nt_sequence,3))     #coding sequence
+    codons_to_remove = ['TGA', 'TAA', 'TAG']     #list of stop codons
+    filtered_codons = [codon for codon in coding_sequence if codon not in codons_to_remove]     # Remove stop codons
+    return(filtered_codons)
+
+def mutate_position_based_on_mutation_rate_p(p_mutation_rate):
+    """This function returns a boolean probability of a mutation based off a given mutation rate p
+    p_mutation_rate = 1 - Cfrac(a,B)**(1/k)
+    Cfrac is the containment index between two sequences"""
+    p = random.random()
+    if p <= p_mutation_rate:
+        return(True)
+    else:
+        return(False)
+
+def mutate_with_nucleotides(mutate_nucleotide):
+    """If the probability is that a mutation occurs, 
+    then this function makes sure that the random choice of a nucloetide mutation is not the same nucleotide of the position being changed.
+    This function removes the nucleotide that will be changed from the nucleotide mutation choices
+    and returns a list of three nucleotides that does not include the nucleotide to be mutated.
+    The function takes in a nucleotide. 
+    nucleotide: the nucleotide to be mutated"""
+    nucleotides = ['A','G','C','T']
+    nucleotides.remove(mutate_nucleotide)
+    return(nucleotides)
+
+def mutate_position(nt_position, p_mutation_rate):
+    prate = mutate_position_based_on_mutation_rate_p(p_mutation_rate)
+    if prate:
+        mutate_with = random.choice(mutate_with_nucleotides(nt_position.upper()))
+        return(mutate_with)
+    else:
+        return(nt_position)
+
+def positive_selection_outcome(codon,p_mutation_rate):
+    mutated_codon = ''
+    if random.choice([0, 1, 2])==random.choice([0, 1, 2]): 
+        for position in range(len(codon)):
+            nt_position = codon[position]
+            if position == 0 or position == 1:
+                mutated_codon += mutate_position(nt_position, p_mutation_rate)
+            elif position == 2:
+                mutated_codon += mutate_position(nt_position, p_mutation_rate)
+    else:
+        for position in range(len(codon)):
+            nt_position = codon[position]
+            if position == 0 or position == 1:
+                mutated_codon += mutate_position(nt_position, p_mutation_rate)
+            else:
+                mutated_codon+=nt_position
+    return(mutated_codon)
+
+def negative_selection_outcome(codon,p_mutation_rate):
+    mutated_codon = ''
+    if random.choice([0, 1, 2])==random.choice([0, 1, 2]): 
+        for position in range(len(codon)):
+            nt_position = codon[position]
+            if position == 0 or position == 1:
+                mutated_codon += mutate_position(nt_position, p_mutation_rate)
+            elif position == 2:
+                mutated_codon += mutate_position(nt_position, p_mutation_rate)
+    else:
+        for position in range(len(codon)):
+            nt_position = codon[position]
+            if position == 2:
+                mutated_codon += mutate_position(nt_position, p_mutation_rate)
+            else:
+                mutated_codon+=nt_position
+    return(mutated_codon)
+
+def positive_selection_based_on_mutation_rate_p(sequence,p_mutation_rate):
+    """This function takes in a sequence to be mutated based on the mutation rate p, which is another argument of the function. 
+    The function loops through each position of the sequence,
+    decides whether the nucleotide at that position is mutated, 
+    and adds on to the newly mutated sequence, which will then be return in the end."""
+    mutated_sequence = ''
+    coding_sequence = list(sliced(sequence,3))
+    for codon in coding_sequence:
+        mutated_sequence+=positive_selection_outcome(codon.upper(), p_mutation_rate)
+    return(mutated_sequence.upper())
+
+def negative_selection_based_on_mutation_rate_p(sequence,p_mutation_rate):
+    """This function takes in a sequence to be mutated based on the mutation rate p, which is another argument of the function. 
+    The function loops through each position of the sequence,
+    decides whether the nucleotide at that position is mutated, 
+    and adds on to the newly mutated sequence, which will then be return in the end."""
+    mutated_sequence = ''
+    coding_sequence = list(sliced(sequence,3))
+    for codon in coding_sequence:
+        mutated_sequence+=negative_selection_outcome(codon.upper(), p_mutation_rate)
+    return(mutated_sequence.upper())
