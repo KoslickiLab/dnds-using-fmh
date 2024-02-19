1174a20d1d16be02b66af173fb0a3e566101c3b0
Judith Rodriguez <judiths.r91@gmail.com>
2024-02-19 18:37:42 -0500

    update script command

--- src/run_fmh_omega.sh
+++ src/run_fmh_omega.sh
@@ -2,12 +2,12 @@
 set -eoux pipefail
 
 #Parameters
-wd=/data/jzr5814/sourmash_dnds_estimation/tests/results/genomic_dnds/gKaKs_paper
-klist='5,7,9,11,13'
+wd=/data/jzr5814/sourmash_dnds_estimation/tests/test/branchwater_test/positive_subsets_0.01
+klist='5,7'
 scaled=1
 cds_input_list=${wd}/fasta_files.txt
-out=gKaKs_paper
-multiple=yes #choose yes if you have multiple fastn files
+m="branchwater" #branchwater mode, single fasta file use sourmash sketch, multiple fasta files to use sourmash sketch
+outname=gKaKs_paper
 
 #Run FMH Omega
-python3 script_fmh_omega.py --cds_input ${cds_input_list} --scaled_input ${scaled} --klist ${klist} --multiple ${multiple} --outname ${out} --working_dir ${wd}
+python3 script_fmh_omega.py --fasta_input_list ${cds_input_list} --scaled_input ${scaled} --klist ${klist} --mode ${m} --outname ${out} --working_dir ${wd}
