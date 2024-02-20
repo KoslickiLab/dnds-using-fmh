0c16888d269e6ba21f0b41b816f56363d28e30b2
Judith Rodriguez <judiths.r91@gmail.com>
2024-02-20 11:49:17 -0500

    update command

--- src/run_fmh_omega.sh
+++ src/run_fmh_omega.sh
@@ -2,9 +2,10 @@
 set -eoux pipefail
 
 #Parameters
-wd=/data/jzr5814/repositories/dnds-using-fmh/src/demo/branchwater
-klist='5,7'
+wd=/data/jzr5814/sourmash_dnds_estimation/tests/results/genomic_dnds/gKaKs_paper/branchwater
+klist='5,7,9,11,13'
 scaled=1
+translate_cds='no' #indicate with yes or no
 
 #Note that if using branchwater mode, makesure working directory is at the beginning of filename. THIS IS NOT REQUIERED FOR OTHER MODES
 cds_input_list=${wd}/datasets.csv
@@ -13,4 +14,4 @@ m="branchwater" #branchwater mode, single fasta file use sourmash sketch, multip
 out=demo_branchwater
 
 #Run FMH Omega
-python3 script_fmh_omega.py --fasta_input_list ${cds_input_list} --scaled_input ${scaled} --klist ${klist} --mode ${m} --outname ${out} --directory ${wd} 
+python3 script_fmh_omega.py --fasta_input_list ${cds_input_list} --scaled_input ${scaled} --klist ${klist} --mode ${m} --translate ${translate_cds} --outname ${out} --directory ${wd} 
