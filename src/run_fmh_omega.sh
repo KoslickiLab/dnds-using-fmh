f09d037cafca5f0abefc08fa7e9066445c821eb7
Judith Rodriguez <judiths.r91@gmail.com>
2024-02-20 10:46:37 -0500

    added information on file that lists input

--- src/run_fmh_omega.sh
+++ src/run_fmh_omega.sh
@@ -2,12 +2,15 @@
 set -eoux pipefail
 
 #Parameters
-wd=/data/jzr5814/sourmash_dnds_estimation/tests/test/branchwater_test/positive_subsets_0.01
+wd=/data/jzr5814/repositories/dnds-using-fmh/src/demo/branchwater
 klist='5,7'
 scaled=1
-cds_input_list=${wd}/fasta_files.txt
+
+#Note that if using branchwater mode, makesure working directory is at the beginning of filename. THIS IS NOT REQUIERED FOR OTHER MODES
+cds_input_list=${wd}/datasets.csv
+
 m="branchwater" #branchwater mode, single fasta file use sourmash sketch, multiple fasta files to use sourmash sketch
-outname=gKaKs_paper
+out=demo_branchwater
 
 #Run FMH Omega
-python3 script_fmh_omega.py --fasta_input_list ${cds_input_list} --scaled_input ${scaled} --klist ${klist} --mode ${m} --outname ${out} --working_dir ${wd}
+python3 script_fmh_omega.py --fasta_input_list ${cds_input_list} --scaled_input ${scaled} --klist ${klist} --mode ${m} --outname ${out} --directory ${wd} 
