7501f67e46762ea495454ad49948954dc0e6afbe
Judith Rodriguez <judiths.r91@gmail.com>
2024-02-21 17:06:51 -0500

    job for GTDB

--- src/run_fmh_omega.sh
+++ src/run_fmh_omega.sh
@@ -2,13 +2,13 @@
 set -eoux pipefail
 
 #Parameters
-wd=/data/jzr5814/sourmash_dnds_estimation/tests/results/genomic_dnds/gKaKs_paper/branchwater
-klist='5,7,9,11,13'
+wd=/data/jzr5814/sourmash_dnds_estimation/tests/results/genomic_dnds/gtdb_protein_rep_multisearch
+klist='9'
 scaled=1
 translate_cds='no' #indicate with yes or no
 
 #Note that if using branchwater mode, makesure working directory is at the beginning of filename. THIS IS NOT REQUIERED FOR OTHER MODES
-cds_input_list=${wd}/datasets.csv
+cds_input_list=${wd}/dataset.csv
 
 m="branchwater" #branchwater mode, single fasta file use sourmash sketch, multiple fasta files to use sourmash sketch
 out=demo_branchwater
