e78b2b89d6726fb77311487634a1b5dd45ba77c5
Judith Rodriguez <judiths.r91@gmail.com>
2024-02-27 10:34:03 -0500

    hard coded 100 total cores for testing gtdb sequences

--- src/run_fmh_omega.sh
+++ src/run_fmh_omega.sh
@@ -2,16 +2,15 @@
 set -eoux pipefail
 
 #Parameters
-wd=/data/jzr5814/sourmash_dnds_estimation/tests/results/genomic_dnds/gtdb_protein_rep_multisearch
-klist='9'
+wd=/data/jzr5814/sourmash_dnds_estimation/tests/results/genomic_dnds/gtdb_protein_rep_pairwise_archaea
+k='5'
 scaled=1
 translate_cds='no' #indicate with yes or no
+m="bwpair" #branchwater mode, single fasta file use sourmash sketch, multiple fasta files to use sourmash sketch
 
 #Note that if using branchwater mode, makesure working directory is at the beginning of filename. THIS IS NOT REQUIERED FOR OTHER MODES
 cds_input_list=${wd}/dataset.csv
-
-m="branchwater" #branchwater mode, single fasta file use sourmash sketch, multiple fasta files to use sourmash sketch
 out=demo_branchwater
 
 #Run FMH Omega
-python3 script_fmh_omega.py --fasta_input_list ${cds_input_list} --scaled_input ${scaled} --klist ${klist} --mode ${m} --translate ${translate_cds} --outname ${out} --directory ${wd} 
+python3 script_fmh_omega.py --fasta_input_list ${cds_input_list} --scaled_input ${scaled} --ksize ${k} --mode ${m} --translate ${translate_cds} --outname ${out} --directory ${wd} 
