8283dc218bf60096a90d4e26e4b8240eaeb32d64
Judith Rodriguez <judiths.r91@gmail.com>
2024-06-26 09:07:33 -0400

    updated repo

--- src/fmh_omega/dnds.py
+++ src/fmh_omega/dnds.py
@@ -66,7 +66,7 @@ def dNdS_ratio_with_constant(protein_containment,nt_containment,k):
         logging.error("ZeroDivisionError: ignore undefined dN/dS estimation")
     return(dNdS_constant)
 
-def report_dNdS(nt_containment_df,prot_containment_df):
+def report_dNdS(nt_containment_df,prot_containment_df,ksize):
     """
     Returns dataframe of dNdS reports between all pairwise estimations of protein coding sequences.
     Uses dNdS_ratio() function to estimate dN/dS ratio.
@@ -77,16 +77,25 @@ def report_dNdS(nt_containment_df,prot_containment_df):
         produced by grab_containment_from_mat() function from reportCI.py. 
         Dataframe contains ref, query, containment index, and ksize.
     """
+    start_time = time.time()
     #read in nt_containment and protein_containment dataframe file and change column names
     nt_df = nt_containment_df.rename(columns={'containment':'DNA_Cfrac'})
     protein_df = prot_containment_df.rename(columns={'containment':'AA_Cfrac'})
     #join df into one
-    df = pd.merge(nt_df, protein_df, on=['A','B','ksize'])
+    df = pd.merge(nt_df, protein_df, on=['A','B'])
+    df['ksize'] = ksize
     #apply function
     df['PdN'] = (calc_PdN(protein_containment=df['AA_Cfrac'],k=df['ksize']))
     df['PdS'] = (calc_PdS(protein_containment=df['AA_Cfrac'],nt_containment=df['DNA_Cfrac'],k=df['ksize']))
     df['PdN/PdS'] = dNdS_ratio(nt_containment=df['DNA_Cfrac'],protein_containment=df['AA_Cfrac'],k=df['ksize'])
     df['dN/dS'] = dNdS_ratio_with_constant(nt_containment=df['DNA_Cfrac'],protein_containment=df['AA_Cfrac'],k=df['ksize'])
+    df['ANI_approx'] = ANI_approx(nt_containment=df['DNA_Cfrac'],k=df['ksize'])
+    df['AAI_approx'] = AAI_approx(protein_containment=df['AA_Cfrac'],k=df['ksize'])
+    
+    end_time = time.time()
+    time_logged = end_time-start_time
+    print(f"Successfully estimated dN/dS in {time_logged} secconds")
+
 
     #report
     return(df)
@@ -103,6 +112,7 @@ def report_dNdS_multisearch(dna_cfrac_csv,protein_cfrac_csv,ksize):
     """
     #read in nt_containment and protein_containment dataframe file and change column names
     #nt_df = nt_containment_df.rename(columns={'containment':'DNA_Cfrac'})
+    start_time = time.time()
     dna_cfrac = pd.read_csv(f'{dna_cfrac_csv}',sep=",")[['query_name','match_name','containment']].rename(columns={'query_name':'A','match_name':'B','containment':'DNA_Cfrac'})
     #protein_df = prot_containment_df.rename(columns={'containment':'AA_Cfrac'})
     protein_cfrac = pd.read_csv(f'{protein_cfrac_csv}',sep=",")[['query_name','match_name','containment']].rename(columns={'query_name':'A','match_name':'B','containment':'AA_Cfrac'})
@@ -117,6 +127,12 @@ def report_dNdS_multisearch(dna_cfrac_csv,protein_cfrac_csv,ksize):
     merge_df['PdS'] = (calc_PdS(protein_containment=merge_df['AA_Cfrac'],nt_containment=merge_df['DNA_Cfrac'],k=merge_df['ksize']))
     merge_df['PdN/PdS'] = dNdS_ratio(nt_containment=merge_df['DNA_Cfrac'],protein_containment=merge_df['AA_Cfrac'],k=merge_df['ksize'])
     merge_df['dN/dS'] = dNdS_ratio_with_constant(nt_containment=merge_df['DNA_Cfrac'],protein_containment=merge_df['AA_Cfrac'],k=merge_df['ksize'])
+    merge_df['ANI_approx'] = ANI_approx(nt_containment=merge_df['DNA_Cfrac'],k=merge_df['ksize'])
+    merge_df['AAI_approx'] = AAI_approx(protein_containment=merge_df['AA_Cfrac'],k=merge_df['ksize'])
+    
+    end_time = time.time()
+    time_logged = end_time-start_time
+    print(f"Successfully estimated dN/dS in {time_logged} secconds")
 
     #report
     return(merge_df)
@@ -141,21 +157,24 @@ def report_dNdS_pairwise(dna_cfrac_csv,protein_cfrac_csv,ksize):
     #protein_df = prot_containment_df.rename(columns={'containment':'AA_Cfrac'})
     protein_cfrac = pd.read_csv(f'{protein_cfrac_csv}',sep=",")[['query_name','match_name','max_containment']].rename(columns={'max_containment':'AA_max_Cfrac'})
     protein_cfrac['A,B'] = protein_cfrac[['query_name', 'match_name']].apply(sorted, axis=1).apply(tuple)
+    #extract columns that you'll use
+    dna_cfrac=dna_cfrac[['A,B','query_name','match_name','DNA_max_Cfrac']].set_index('A,B')
+    protein_cfrac=protein_cfrac[['A,B','AA_max_Cfrac']].set_index('A,B')
     #join df into one
-    merge_df = pd.merge(dna_cfrac, protein_cfrac, on='A,B', how='inner')
-    merge_df['ksize'] = int(ksize)
+    concat_df=pd.concat([dna_cfrac, protein_cfrac], axis=1).reset_index()
+    concat_df['ksize'] = int(ksize)
 
     #apply function
-    merge_df['PdN'] = (calc_PdN(protein_containment=merge_df['AA_max_Cfrac'],k=merge_df['ksize']))
-    merge_df['PdS'] = (calc_PdS(protein_containment=merge_df['AA_max_Cfrac'],nt_containment=merge_df['DNA_max_Cfrac'],k=merge_df['ksize']))
-    merge_df['PdN/PdS'] = dNdS_ratio(nt_containment=merge_df['DNA_max_Cfrac'],protein_containment=merge_df['AA_max_Cfrac'],k=merge_df['ksize'])
-    merge_df['dN/dS'] = dNdS_ratio_with_constant(nt_containment=merge_df['DNA_max_Cfrac'],protein_containment=merge_df['AA_max_Cfrac'],k=merge_df['ksize'])
-    merge_df['ANI_approx'] = ANI_approx(nt_containment=merge_df['DNA_max_Cfrac'],k=merge_df['ksize'])
-    merge_df['AAI_approx'] = AAI_approx(protein_containment=merge_df['AA_max_Cfrac'],k=merge_df['ksize'])
+    concat_df['PdN'] = (calc_PdN(protein_containment=concat_df['AA_max_Cfrac'],k=concat_df['ksize']))
+    concat_df['PdS'] = (calc_PdS(protein_containment=concat_df['AA_max_Cfrac'],nt_containment=concat_df['DNA_max_Cfrac'],k=concat_df['ksize']))
+    concat_df['PdN/PdS'] = dNdS_ratio(nt_containment=concat_df['DNA_max_Cfrac'],protein_containment=concat_df['AA_max_Cfrac'],k=concat_df['ksize'])
+    concat_df['dN/dS'] = dNdS_ratio_with_constant(nt_containment=concat_df['DNA_max_Cfrac'],protein_containment=concat_df['AA_max_Cfrac'],k=concat_df['ksize'])
+    concat_df['ANI_approx'] = ANI_approx(nt_containment=concat_df['DNA_max_Cfrac'],k=concat_df['ksize'])
+    concat_df['AAI_approx'] = AAI_approx(protein_containment=concat_df['AA_max_Cfrac'],k=concat_df['ksize'])
 
     end_time = time.time()
     time_logged = end_time-start_time
     print(f"Successfully estimated dN/dS in {time_logged} secconds")
     #report
-    return(merge_df)
+    return(concat_df)
 
--- src/run_fmh_omega.sh
+++ src/run_fmh_omega.sh
@@ -2,15 +2,16 @@
 set -eoux pipefail
 
 #Parameters
-wd=/data/jzr5814/sourmash_dnds_estimation/tests/results/genomic_dnds/gtdb_protein_rep_pairwise_archaea
+wd=/data/jzr5814/sourmash_dnds_estimation/tests/results/genomic_dnds/gtdb_protein_rep_pairwise_archaea_100cores_100scale
 k='5'
-scaled=1
+scaled=100
 translate_cds='no' #indicate with yes or no
 m="bwpair" #branchwater mode, single fasta file use sourmash sketch, multiple fasta files to use sourmash sketch
+c=100
 
 #Note that if using branchwater mode, makesure working directory is at the beginning of filename. THIS IS NOT REQUIERED FOR OTHER MODES
 cds_input_list=${wd}/dataset.csv
 out=demo_branchwater
 
 #Run FMH Omega
-python3 script_fmh_omega.py --fasta_input_list ${cds_input_list} --scaled_input ${scaled} --ksize ${k} --mode ${m} --translate ${translate_cds} --outname ${out} --directory ${wd} 
+python3 script_fmh_omega.py --fasta_input_list ${cds_input_list} --scaled_input ${scaled} --ksize ${k} --mode ${m} --translate ${translate_cds} --outname ${out} --directory ${wd} --cores ${c} 
