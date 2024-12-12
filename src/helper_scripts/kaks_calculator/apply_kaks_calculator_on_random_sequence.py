020b1cf344cfd2eebddc5547f20afac5b7d6fe98
Judith Rodriguez <judiths.r91@gmail.com>
2024-12-12 10:52:00 -0500

    aupdate --input argument help description

--- src/helper_scripts/kaks_calculator/apply_kaks_calculator_on_random_sequence.py
+++ src/helper_scripts/kaks_calculator/apply_kaks_calculator_on_random_sequence.py
@@ -12,6 +12,7 @@ import subprocess
 import os
 from Bio.SeqUtils.CheckSum import seguid
 from Bio import SeqIO
+from collections import defaultdict
 
 def main(args):
 
@@ -19,23 +20,45 @@ def main(args):
     input_filename = os.path.basename(args.input)
     wd=args.wd.rstrip('/')
     axt_file = open(f'{wd}/{input_filename}.axt','w')
-    
-    seguid_dict = SeqIO.to_dict(SeqIO.parse(input_file, "fasta"), lambda rec: seguid(rec.seq))
 
-    for key in seguid_dict:
-        ref = seguid_dict[key].description
-        ref_seq = str(seguid_dict[key].seq)
+    seq_dict = defaultdict(list)
+    for record in SeqIO.parse(input_file, "fasta"):
+        key = seguid(record.seq)
+        seq_dict[key].append(record)
+
+    if args.short:
+        # Shorter sequences create duplicate keys when running SeqIO.to_dict, Assuming seq_dict contains lists of SeqRecord objects
+        for key, records in seq_dict.items():  # Iterate through each key and list of SeqRecord objects
+            for ref_record in records:  # Loop over each SeqRecord in the list of the current key
+                ref = ref_record.description  # Access the description of the current reference record
+                ref_seq = str(ref_record.seq)  # Access the sequence of the current reference record
+                
+                # Now compare this reference record with every other SeqRecord in seq_dict
+                for other_key, other_records in seq_dict.items():  # Compare with every key in the dictionary
+                    for other_record in other_records:  # Loop through the list of SeqRecord objects for the other key
+                        key_name = str(other_record.description)  # Get the description of the other SeqRecord
+                        
+                        # Print and write comparison information
+                        axt_file.write(f'{ref}_vs_{key_name}\n')  # Write the comparison to the file
+                        axt_file.write(ref_seq + '\n')  # Write the reference sequence to the file
+                        axt_file.write(str(other_record.seq) + '\n')  # Write the other sequence to the file
+                        axt_file.write('\n')  # Write an empty line to separate comparison
+
+    else:
+        seguid_dict = SeqIO.to_dict(SeqIO.parse(input_file, "fasta"), lambda rec: seguid(rec.seq))
         for key in seguid_dict:
-            key_name = str(seguid_dict[key].description)
-            print(f'{ref}_vs_{key_name}')
-            axt_file.write(f'{ref}_vs_{key_name}')
-            axt_file.write('\n')
-            axt_file.write(ref_seq)
-            axt_file.write('\n')
-            axt_file.write(str(seguid_dict[key].seq))
-            axt_file.write('\n')
-            axt_file.write('\n')
-    
+            ref = seguid_dict[key].description
+            ref_seq = str(seguid_dict[key].seq)
+            for key in seguid_dict:
+                key_name = str(seguid_dict[key].description)
+                axt_file.write(f'{ref}_vs_{key_name}')
+                axt_file.write('\n')
+                axt_file.write(ref_seq)
+                axt_file.write('\n')
+                axt_file.write(str(seguid_dict[key].seq))
+                axt_file.write('\n')
+                axt_file.write('\n')
+
     cmd=f'KaKs_Calculator -i {wd}/{input_filename}.axt -o {wd}/{input_filename}.axt.kaks -m {args.method}'
     subprocess.run(cmd, shell=True, check=True)
 
@@ -60,6 +83,12 @@ if __name__ == "__main__":
         '--wd',
         type=str,
         help = 'Identify the working directory where output files will be produced.'
+    )
+    
+    parser.add_argument(
+        '--short',
+        action='store_true',  # This makes the argument a boolean flag
+        help='When applying on shorter sequences, less than 1000 nucleotides long (default: False)'
     )
 
     args = parser.parse_args()
