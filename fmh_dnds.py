69de404af72d1b5efe0c20586dbed338f8d37758
Judith Rodriguez <judiths.r91@gmail.com>
2026-08-27 11:42:26 -0400

    change import foldername for functions

--- fmh_dnds.py
+++ fmh_dnds.py
@@ -2,7 +2,7 @@
 """New approach to estimaating dN/dS ratio of metagenomic data"""
 
 import argparse
-from fmh_dnds import helperfuncs,dnds,sourmash_ext
+from src import helperfuncs,dnds,sourmash_ext
 import subprocess
 
 def main(args):
